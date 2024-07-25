47e11ffe5c24d3661f05214a284934ea824ada48
atsushi421 <atsushi.yano.2@tier4.jp>
2024-07-25 12:08:04 +0900

    fix: leak_warn_threshold
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

--- kmod/agnocast.c
+++ kmod/agnocast.c
@@ -403,7 +403,7 @@ static uint64_t try_release_removable_oldest_message(const char *topic_name, uin
 
 	if (publisher_queue->queue_size <= qos_depth) return 0;
 
-	uint32_t leak_warn_threshold = (qos_depth <= 100) ? 100 : qos_depth * 2;  // This is rough value.
+	uint32_t leak_warn_threshold = (qos_depth <= 100) ? 100 + qos_depth : qos_depth * 2;  // This is rough value.
 	if (publisher_queue->queue_size > leak_warn_threshold) {
 		printk(KERN_WARNING "For some reason the reference count of the message is not reduced and the queue size is huge: publisher queue publisher_pid=%d, topic_name=%s (try_release_removable_oldest_message)\n", publisher_pid, topic_name);
 	}
