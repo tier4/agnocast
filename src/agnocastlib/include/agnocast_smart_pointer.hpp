10a3c46f7370647ae1b24b603ec957bc80927ca3
Ryuta Kambe <ryuta.kambe@tier4.jp>
2024-08-27 11:06:52 +0900

    fix: modify smart pointer functions (#132)
    * fix: increment/decrement rc only for subscribers
    
    Signed-off-by: veqcc <ryuta.kambe@tier4.jp>
    
    * fix typo
    
    Signed-off-by: veqcc <ryuta.kambe@tier4.jp>
    
    * fix variable name
    
    Signed-off-by: veqcc <ryuta.kambe@tier4.jp>
    
    ---------
    
    Signed-off-by: veqcc <ryuta.kambe@tier4.jp>

--- src/agnocastlib/include/agnocast_smart_pointer.hpp
+++ src/agnocastlib/include/agnocast_smart_pointer.hpp
@@ -14,6 +14,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <iostream>
 
 namespace agnocast
 {
@@ -27,10 +28,12 @@ class message_ptr
   const char * topic_name_;
   uint32_t publisher_pid_;
   uint64_t timestamp_;
+  bool need_rc_update_;
 
   void release()
   {
     if (ptr_ == nullptr) return;
+    if (!need_rc_update_) return;
 
     union ioctl_update_entry_args entry_args;
     entry_args.topic_name = topic_name_;
@@ -46,20 +49,6 @@ class message_ptr
     ptr_ = nullptr;
   }
 
-  void increment_rc()
-  {
-    union ioctl_update_entry_args entry_args;
-    entry_args.topic_name = topic_name_;
-    entry_args.subscriber_pid = getpid();
-    entry_args.publisher_pid = publisher_pid_;
-    entry_args.msg_timestamp = timestamp_;
-    if (ioctl(agnocast_fd, AGNOCAST_INCREMENT_RC_CMD, &entry_args) < 0) {
-      perror("AGNOCAST_INCREMENT_RC_CMD failed");
-      close(agnocast_fd);
-      exit(EXIT_FAILURE);
-    }
-  }
-
 public:
   const char * get_topic_name() { return topic_name_; }
   uint32_t get_publisher_pid() const { return publisher_pid_; }
@@ -69,12 +58,13 @@ public:
 
   explicit message_ptr(
     T * ptr, const char * topic_name, uint32_t publisher_pid, uint64_t timestamp,
-    bool already_rc_incremented = false)
-  : ptr_(ptr), topic_name_(topic_name), publisher_pid_(publisher_pid), timestamp_(timestamp)
+    bool need_rc_update)
+  : ptr_(ptr),
+    topic_name_(topic_name),
+    publisher_pid_(publisher_pid),
+    timestamp_(timestamp),
+    need_rc_update_(need_rc_update)
   {
-    if (!already_rc_incremented) {
-      increment_rc();
-    }
   }
 
   ~message_ptr() { release(); }
@@ -85,21 +75,16 @@ public:
     publisher_pid_(r.publisher_pid_),
     timestamp_(r.timestamp_)
   {
-    increment_rc();
+    std::cout << "[Error]: copy constructor is not supported yet" << std::endl;
+    close(agnocast_fd);
+    exit(EXIT_FAILURE);
   }
 
   message_ptr & operator=(const message_ptr & r)
   {
-    if (this == &r) return *this;
-
-    release();
-
-    ptr_ = r.ptr_;
-    topic_name_ = r.topic_name_;
-    publisher_pid_ = r.publisher_pid_;
-    timestamp_ = r.timestamp_;
-
-    increment_rc();
+    std::cout << "[Error]: copy assignment operator is not supported yet" << std::endl;
+    close(agnocast_fd);
+    exit(EXIT_FAILURE);
   }
 
   message_ptr(message_ptr && r)
@@ -108,22 +93,24 @@ public:
     publisher_pid_(r.publisher_pid_),
     timestamp_(r.timestamp_)
   {
-    r.ptr_ = nullptr;
+    std::cout << "[Error]: move constructor is not supported yet" << std::endl;
+    close(agnocast_fd);
+    exit(EXIT_FAILURE);
   }
 
   message_ptr & operator=(message_ptr && r)
   {
-    release();
-
-    ptr_ = r.ptr_;
-    topic_name_ = r.topic_name_;
-    publisher_pid_ = r.publisher_pid_;
-    timestamp_ = r.timestamp_;
-
-    r.ptr_ = nullptr;
+    std::cout << "[Error]: move assignment operator is not supported yet" << std::endl;
+    close(agnocast_fd);
+    exit(EXIT_FAILURE);
   }
 
-  T & operator*() const noexcept { return *ptr_; }
+  T & operator*() const noexcept
+  {
+    std::cout << "[Error]: operator*() is not supported yet" << std::endl;
+    close(agnocast_fd);
+    exit(EXIT_FAILURE);
+  }
 
   T * operator->() const noexcept { return ptr_; }
 
