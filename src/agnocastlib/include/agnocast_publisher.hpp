634ab0aa1fa924a59e348ee78b789b1402f3b995
atsushi yano <55824710+atsushi421@users.noreply.github.com>
2024-10-15 11:27:47 +0900

    refactor(Publisher): cut out methods (#242)
    * refactor: cut out process of Publisher
    
    * fix: cppcheck
    
    * refactor: cut out borrow_loaned_message()
    
    * fix: cppcheck

--- src/agnocastlib/include/agnocast_publisher.hpp
+++ src/agnocastlib/include/agnocast_publisher.hpp
@@ -22,9 +22,14 @@ namespace agnocast
 
 extern int agnocast_fd;
 uint64_t agnocast_get_timestamp();
-void validate_ld_preload();
 
-std::string create_mq_name(const std::string & topic_name, const uint32_t pid);
+// These are cut out of the class for information hiding.
+void initialize_publisher(uint32_t publisher_pid, const std::string & topic_name);
+void publish_core(const std::string & topic_name, uint32_t publisher_pid, uint64_t timestamp);
+uint32_t get_subscription_count_core(const std::string & topic_name);
+std::vector<uint64_t> borrow_loaned_message_core(
+  const std::string & topic_name, uint32_t publisher_pid, uint32_t qos_depth,
+  uint64_t msg_virtual_address, uint64_t timestamp);
 
 template <typename MessageT>
 class Publisher
@@ -40,56 +45,9 @@ public:
   using SharedPtr = std::shared_ptr<Publisher<MessageT>>;
 
   Publisher(const std::string & topic_name, const rclcpp::QoS & qos)
-  : topic_name_(topic_name), qos_(qos)
+  : topic_name_(topic_name), publisher_pid_(getpid()), qos_(qos)
   {
-    validate_ld_preload();
-
-    publisher_pid_ = getpid();
-
-    union ioctl_publisher_args pub_args;
-    pub_args.publisher_pid = publisher_pid_;
-    pub_args.topic_name = topic_name_.c_str();
-    if (ioctl(agnocast_fd, AGNOCAST_PUBLISHER_ADD_CMD, &pub_args) < 0) {
-      RCLCPP_ERROR(logger, "AGNOCAST_PUBLISHER_ADD_CMD failed: %s", strerror(errno));
-      close(agnocast_fd);
-      exit(EXIT_FAILURE);
-    }
-
-    // Send messages to subscribers to notify that a new publisher appears
-    for (uint32_t i = 0; i < pub_args.ret_subscriber_len; i++) {
-      if (pub_args.ret_subscriber_pids[i] == publisher_pid_) {
-        /*
-         * NOTE: In ROS2, communication should work fine even if the same process exists as both a
-         * publisher and a subscriber for a given topic. However, in Agnocast, to avoid applying
-         * Agnocast to topic communication within a component container, the system will explicitly
-         * fail with an error during initialization.
-         */
-        RCLCPP_ERROR(
-          logger,
-          "This process (pid=%d) already exists in the topic (topic_name=%s) "
-          "as a publisher.",
-          publisher_pid_, topic_name.c_str());
-        exit(EXIT_FAILURE);
-      }
-      const std::string mq_name =
-        "/new_publisher@" + std::to_string(pub_args.ret_subscriber_pids[i]);
-      mqd_t mq = mq_open(mq_name.c_str(), O_WRONLY);
-      if (mq == -1) {
-        RCLCPP_ERROR(logger, "mq_open for new publisher failed: %s", strerror(errno));
-        close(agnocast_fd);
-        exit(EXIT_FAILURE);
-      }
-
-      MqMsgNewPublisher mq_msg;
-      mq_msg.publisher_pid = publisher_pid_;
-      mq_msg.shm_addr = pub_args.ret_shm_addr;
-      mq_msg.shm_size = pub_args.ret_shm_size;
-      if (mq_send(mq, reinterpret_cast<char *>(&mq_msg), sizeof(mq_msg), 0) == -1) {
-        RCLCPP_ERROR(logger, "mq_send for new publisher failed: %s", strerror(errno));
-        close(agnocast_fd);
-        exit(EXIT_FAILURE);
-      }
-    }
+    initialize_publisher(publisher_pid_, topic_name_);
   }
 
   ipc_shared_ptr<MessageT> borrow_loaned_message()
@@ -101,20 +59,12 @@ public:
   ipc_shared_ptr<MessageT> borrow_loaned_message(MessageT * ptr)
   {
     uint64_t timestamp = agnocast_get_timestamp();
-    union ioctl_enqueue_and_release_args ioctl_args;
-    ioctl_args.topic_name = topic_name_.c_str();
-    ioctl_args.publisher_pid = publisher_pid_;
-    ioctl_args.qos_depth = static_cast<uint32_t>(qos_.depth());
-    ioctl_args.msg_virtual_address = reinterpret_cast<uint64_t>(ptr);
-    ioctl_args.timestamp = timestamp;
-    if (ioctl(agnocast_fd, AGNOCAST_ENQUEUE_AND_RELEASE_CMD, &ioctl_args) < 0) {
-      RCLCPP_ERROR(logger, "AGNOCAST_ENQUEUE_AND_RELEASE_CMD failed: %s", strerror(errno));
-      close(agnocast_fd);
-      exit(EXIT_FAILURE);
-    }
+    std::vector<uint64_t> released_addrs = borrow_loaned_message_core(
+      topic_name_, publisher_pid_, static_cast<uint32_t>(qos_.depth()),
+      reinterpret_cast<uint64_t>(ptr), timestamp);
 
-    for (size_t i = 0; i < ioctl_args.ret_len; i++) {
-      MessageT * release_ptr = reinterpret_cast<MessageT *>(ioctl_args.ret_released_addrs[i]);
+    for (uint64_t addr : released_addrs) {
+      MessageT * release_ptr = reinterpret_cast<MessageT *>(addr);
       delete release_ptr;
     }
 
@@ -126,56 +76,10 @@ public:
     if (topic_name_.c_str() != message.get_topic_name()) return;  // string comparison?
     if (publisher_pid_ != message.get_publisher_pid()) return;
 
-    union ioctl_publish_args publish_args;
-    publish_args.topic_name = topic_name_.c_str();
-    publish_args.publisher_pid = publisher_pid_;
-    publish_args.msg_timestamp = message.get_timestamp();
-    if (ioctl(agnocast_fd, AGNOCAST_PUBLISH_MSG_CMD, &publish_args) < 0) {
-      RCLCPP_ERROR(logger, "AGNOCAST_PUBLISH_MSG_CMD failed: %s", strerror(errno));
-      close(agnocast_fd);
-      exit(EXIT_FAILURE);
-    }
-
-    for (uint32_t i = 0; i < publish_args.ret_len; i++) {
-      uint32_t pid = publish_args.ret_pids[i];
-
-      std::string mq_name = create_mq_name(topic_name_.c_str(), pid);
-      mqd_t mq;
-      if (opened_mqs.find(mq_name) != opened_mqs.end()) {
-        mq = opened_mqs[mq_name];
-      } else {
-        mq = mq_open(mq_name.c_str(), O_WRONLY | O_NONBLOCK);
-        if (mq == -1) {
-          RCLCPP_ERROR(logger, "mq_open failed: %s", strerror(errno));
-          continue;
-        }
-        opened_mqs.insert({mq_name, mq});
-      }
-
-      struct MqMsgAgnocast mq_msg;
-      if (mq_send(mq, reinterpret_cast<char *>(&mq_msg), sizeof(mq_msg), 0) == -1) {
-        // If it returns EAGAIN, it means mq_send has already been executed, but the subscriber
-        // hasn't received it yet. Thus, there's no need to send it again since the notification has
-        // already been sent.
-        if (errno != EAGAIN) {
-          RCLCPP_ERROR(logger, "mq_send failed: %s", strerror(errno));
-        }
-      }
-    }
+    publish_core(topic_name_, publisher_pid_, message.get_timestamp());
   }
 
-  uint32_t get_subscription_count() const
-  {
-    union ioctl_get_subscriber_num_args get_subscriber_count_args;
-    get_subscriber_count_args.topic_name = topic_name_.c_str();
-    if (ioctl(agnocast_fd, AGNOCAST_GET_SUBSCRIBER_NUM_CMD, &get_subscriber_count_args) < 0) {
-      RCLCPP_ERROR(logger, "AGNOCAST_GET_SUBSCRIBER_NUM_CMD failed: %s", strerror(errno));
-      close(agnocast_fd);
-      exit(EXIT_FAILURE);
-    }
-
-    return get_subscriber_count_args.ret_subscriber_num;
-  }
+  uint32_t get_subscription_count() const { return get_subscription_count_core(topic_name_); }
 };
 
 }  // namespace agnocast
