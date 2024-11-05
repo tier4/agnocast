be990dc7dcffdf0837aa7a52061e1c5f6a35dfa6
Takahiro Ishikawa <sykwer@gmail.com>
2024-11-05 21:15:10 +0900

    add abstract agnocast executor class
    Signed-off-by: Takahiro Ishikawa <sykwer@gmail.com>

---
+++ src/agnocastlib/include/agnocast_executor.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "agnocast_topic_info.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+#include <queue>
+
+namespace agnocast
+{
+
+struct AgnocastExecutables
+{
+  std::queue<std::shared_ptr<std::function<void()>>> callable_queue;
+};
+
+class AgnocastExecutor : public rclcpp::Executor
+{
+  // prevent objects from being destructed by keeping reference count
+  std::vector<rclcpp::Node::SharedPtr> nodes_;
+
+protected:
+  int epoll_fd_;
+  pid_t my_pid_;
+
+  void prepare_epoll();
+  bool get_next_agnocast_executables(
+    AgnocastExecutables & agnocast_executables, const int timeout_ms);
+  void execute_agnocast_executables(AgnocastExecutables & agnocast_executables);
+
+public:
+  RCLCPP_PUBLIC
+  explicit AgnocastExecutor(const rclcpp::ExecutorOptions & options = rclcpp::ExecutorOptions());
+
+  RCLCPP_PUBLIC
+  virtual ~AgnocastExecutor();
+
+  void add_node(rclcpp::Node::SharedPtr node, bool notify = true) override;
+
+  virtual void spin() = 0;
+};
+
+}  // namespace agnocast
