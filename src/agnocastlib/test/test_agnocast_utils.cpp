3d1f3aeb29c43d8cfded93600322f8ec5a0959ed
atsushi yano <55824710+atsushi421@users.noreply.github.com>
2024-11-18 11:13:40 +0900

    test: create_mq_name (#272)

--- src/agnocastlib/test/test_agnocast_utils.cpp
+++ src/agnocastlib/test/test_agnocast_utils.cpp
@@ -2,8 +2,17 @@
 
 #include <gtest/gtest.h>
 
-TEST(AgnocastUtilsTest, create_mq_name)
+TEST(AgnocastUtilsTest, create_mq_name_normal)
 {
-  std::string mq_name = agnocast::create_mq_name("/test", 0);
-  EXPECT_EQ(mq_name, "/test@0");
+  EXPECT_EQ(agnocast::create_mq_name("/dummy", 0), "/dummy@0");
+}
+
+TEST(AgnocastUtilsTest, create_mq_name_slash_included)
+{
+  EXPECT_EQ(agnocast::create_mq_name("/dummy/dummy", 0), "/dummy_dummy@0");
+}
+
+TEST(AgnocastUtilsTest, create_mq_name_invalid_topic)
+{
+  EXPECT_EXIT(agnocast::create_mq_name("dummy", 0), ::testing::ExitedWithCode(EXIT_FAILURE), "");
 }
