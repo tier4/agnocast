f2804ff3da16b8a8cda24953def93ac98e27d387
atsushi yano <55824710+atsushi421@users.noreply.github.com>
2024-11-19 10:30:59 +0900

    test: unit test for agnocast_topic_info (#277)

---
+++ src/agnocastlib/test/test_agnocast_topic_info.cpp
@@ -0,0 +1,39 @@
+#include "agnocast_topic_info.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(AgnocastTopicInfoTest, callback_first_arg)
+{
+  using FuncType = std::function<void(int, float)>;
+  using FirstArgType = agnocast::callback_first_arg<FuncType>::type;
+  EXPECT_TRUE((std::is_same<FirstArgType, int>::value));
+}
+
+TEST(AgnocastTopicInfoTest, get_erased_callback_normal)
+{
+  bool callback_called = false;
+  int data = 0;
+  agnocast::TypedMessagePtr<int> int_arg{
+    agnocast::ipc_shared_ptr<int>(&data, "dummy", 0, 0, false)};
+  auto int_callback = [&](agnocast::ipc_shared_ptr<int> /*unused_arg*/) { callback_called = true; };
+
+  agnocast::TypeErasedCallback erased_callback = agnocast::get_erased_callback<int>(int_callback);
+  erased_callback(int_arg);
+
+  EXPECT_TRUE(callback_called);
+}
+
+TEST(AgnocastTopicInfoTest, get_erased_callback_invalid_type)
+{
+  int data = 0;
+  agnocast::TypedMessagePtr<int> int_arg{
+    agnocast::ipc_shared_ptr<int>(&data, "dummy", 0, 0, false)};
+  auto float_callback = [&](agnocast::ipc_shared_ptr<float> /*unused_arg*/) {};
+
+  agnocast::TypeErasedCallback erased_callback =
+    agnocast::get_erased_callback<float>(float_callback);
+
+  EXPECT_EXIT(
+    erased_callback(int_arg), ::testing::ExitedWithCode(EXIT_FAILURE),
+    "Agnocast internal implementation error: bad allocation when callback is called");
+}
