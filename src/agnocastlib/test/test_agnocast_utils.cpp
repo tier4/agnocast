19f4bfa5dd65086644401f90db8838b64f8764c5
atsushi yano <55824710+atsushi421@users.noreply.github.com>
2024-12-04 03:40:46 +0900

    feat: enable co-operation with LD_PRELOAD apps (#308)
    * feat: enable to co-operation with LD_PRELOAD apps
    
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>
    
    * fix
    
    * Update docs/how_to_set_environment_variables.md
    
    Co-authored-by: Koichi Imai <45482193+Koichi98@users.noreply.github.com>
    
    * Update docs/how_to_set_environment_variables.md
    
    Co-authored-by: Koichi Imai <45482193+Koichi98@users.noreply.github.com>
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>
    Co-authored-by: Koichi Imai <45482193+Koichi98@users.noreply.github.com>

--- src/agnocastlib/test/test_agnocast_utils.cpp
+++ src/agnocastlib/test/test_agnocast_utils.cpp
@@ -19,41 +19,41 @@ TEST(AgnocastUtilsTest, create_mq_name_invalid_topic)
 
 TEST(AgnocastUtilsTest, validate_ld_preload_normal)
 {
-  setenv("LD_PRELOAD", "libagnocast_heaphook.so", 1);
+  setenv("LD_PRELOAD", "libagnocast_heaphook.so:", 1);
   EXPECT_NO_THROW(agnocast::validate_ld_preload());
   unsetenv("LD_PRELOAD");
 }
 
 TEST(AgnocastUtilsTest, validate_ld_preload_nothing)
 {
-  EXPECT_EXIT(
-    agnocast::validate_ld_preload(), ::testing::ExitedWithCode(EXIT_FAILURE),
-    "LD_PRELOAD is not set to libagnocast_heaphook.so");
+  EXPECT_EXIT(agnocast::validate_ld_preload(), ::testing::ExitedWithCode(EXIT_FAILURE), "");
 }
 
 TEST(AgnocastUtilsTest, validate_ld_preload_different)
 {
   setenv("LD_PRELOAD", "dummy", 1);
-  EXPECT_EXIT(
-    agnocast::validate_ld_preload(), ::testing::ExitedWithCode(EXIT_FAILURE),
-    "LD_PRELOAD is not set to libagnocast_heaphook.so");
+  EXPECT_EXIT(agnocast::validate_ld_preload(), ::testing::ExitedWithCode(EXIT_FAILURE), "");
   unsetenv("LD_PRELOAD");
 }
 
 TEST(AgnocastUtilsTest, validate_ld_preload_suffix)
 {
-  setenv("LD_PRELOAD", "libagnocast_heaphook.so_dummy", 1);
-  EXPECT_EXIT(
-    agnocast::validate_ld_preload(), ::testing::ExitedWithCode(EXIT_FAILURE),
-    "LD_PRELOAD is not set to libagnocast_heaphook.so");
+  setenv("LD_PRELOAD", "libagnocast_heaphook.so:dummy", 1);
+  EXPECT_NO_THROW(agnocast::validate_ld_preload());
   unsetenv("LD_PRELOAD");
 }
 
 TEST(AgnocastUtilsTest, validate_ld_preload_prefix)
 {
-  setenv("LD_PRELOAD", "dummy_libagnocast_heaphook.so", 1);
-  EXPECT_EXIT(
-    agnocast::validate_ld_preload(), ::testing::ExitedWithCode(EXIT_FAILURE),
-    "LD_PRELOAD is not set to libagnocast_heaphook.so");
+  setenv("LD_PRELOAD", "dummy:libagnocast_heaphook.so", 1);
+  EXPECT_NO_THROW(agnocast::validate_ld_preload());
+  unsetenv("LD_PRELOAD");
+}
+
+TEST(AgnocastUtilsTest, validate_ld_preload_only_libagnocast_heaphook)
+{
+  setenv("LD_PRELOAD", "libagnocast_heaphook.so", 1);
+  setenv("MEMPOOL_SIZE", "10000000", 1);
+  EXPECT_EXIT(agnocast::validate_ld_preload(), ::testing::ExitedWithCode(EXIT_FAILURE), "");
   unsetenv("LD_PRELOAD");
 }
