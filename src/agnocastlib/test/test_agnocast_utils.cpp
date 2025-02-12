3ad43e7a6a73061f2067a4a86c8f7e9494e5c69e
Koichi Imai <koichi.imai.2@tier4.jp>
2025-02-12 16:13:15 +0900

    add agnocast to mq_name
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- src/agnocastlib/test/test_agnocast_utils.cpp
+++ src/agnocastlib/test/test_agnocast_utils.cpp
@@ -4,12 +4,12 @@
 
 TEST(AgnocastUtilsTest, create_mq_name_normal)
 {
-  EXPECT_EQ(agnocast::create_mq_name("/dummy", 0), "/dummy@0");
+  EXPECT_EQ(agnocast::create_mq_name("/dummy", 0), "/agnocast@dummy@0");
 }
 
 TEST(AgnocastUtilsTest, create_mq_name_slash_included)
 {
-  EXPECT_EQ(agnocast::create_mq_name("/dummy/dummy", 0), "/dummy_dummy@0");
+  EXPECT_EQ(agnocast::create_mq_name("/dummy/dummy", 0), "/agnocast@dummy_dummy@0");
 }
 
 TEST(AgnocastUtilsTest, create_mq_name_invalid_topic)
