feda6094d16fcb8e97863a333974af947a596312
veqcc <ryuta.kambe@tier4.jp>
2025-02-13 10:22:56 +0900

    fix(agnocastlib): remove unnecessary mq
    Signed-off-by: veqcc <ryuta.kambe@tier4.jp>

--- src/agnocastlib/include/agnocast_mq.hpp
+++ src/agnocastlib/include/agnocast_mq.hpp
@@ -1,16 +1,7 @@
 #pragma once
 
-#include <cstdint>
-
 namespace agnocast
 {
-
-struct MqMsgNewPublisher
-{
-  pid_t publisher_pid;
-  uint64_t shm_addr;
-  uint64_t shm_size;
-};
 
 struct MqMsgAgnocast
 {
