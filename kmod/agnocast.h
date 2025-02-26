ac2aef110764382277c9d42099bbb5b532aa573e
TetsuKawa <70682030+TetsuKawa@users.noreply.github.com>
2025-02-26 10:40:25 +0900

    feat(kmod): add some ioctl cmd (#417)
    add some ioctl cmd
    
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>

--- kmod/agnocast.h
+++ kmod/agnocast.h
@@ -141,7 +141,18 @@ union ioctl_topic_list_args {
   uint32_t ret_topic_num;
 };
 
+union ioctl_node_info_args {
+  struct
+  {
+    const char * node_name;
+    uint64_t topic_name_buffer_addr;
+  };
+  uint32_t ret_topic_num;
+};
+
 #define AGNOCAST_GET_TOPIC_LIST_CMD _IOR('R', 1, union ioctl_topic_list_args)
+#define AGNOCAST_GET_NODE_SUBSCRIBER_TOPICS_CMD _IOR('R', 2, union ioctl_node_info_args)
+#define AGNOCAST_GET_NODE_PUBLISHER_TOPICS_CMD _IOR('R', 3, union ioctl_node_info_args)
 
 // ================================================
 // public functions in agnocast.c
