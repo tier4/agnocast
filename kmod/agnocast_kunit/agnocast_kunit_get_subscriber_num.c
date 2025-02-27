0ee7faef4841bc141939930830db94e7c12bbafa
veqcc <ryuta.kambe@tier4.jp>
2025-02-27 09:12:25 +0900

    AAA patern
    Signed-off-by: veqcc <ryuta.kambe@tier4.jp>

--- kmod/agnocast_kunit/agnocast_kunit_get_subscriber_num.c
+++ kmod/agnocast_kunit/agnocast_kunit_get_subscriber_num.c
@@ -14,19 +14,17 @@ void test_case_get_subscriber_num_normal(struct kunit * test)
   bool is_take_sub = false;
 
   union ioctl_new_shm_args new_shm_args;
-  int ret1 = new_shm_addr(subscriber_pid, 1024, &new_shm_args);
+  new_shm_addr(subscriber_pid, 1024, &new_shm_args);
 
   union ioctl_subscriber_args subscriber_args;
-  int ret2 = subscriber_add(
+  subscriber_add(
     topic_name, node_name, qos_depth, qos_is_transient_local, subscriber_pid, is_take_sub,
     &subscriber_args);
 
   union ioctl_get_subscriber_num_args subscriber_num_args;
-  int ret3 = get_subscriber_num(topic_name, &subscriber_num_args);
+  int ret = get_subscriber_num(topic_name, &subscriber_num_args);
 
-  KUNIT_EXPECT_EQ(test, ret1, 0);
-  KUNIT_EXPECT_EQ(test, ret2, 0);
-  KUNIT_EXPECT_EQ(test, ret3, 0);
+  KUNIT_EXPECT_EQ(test, ret, 0);
   KUNIT_EXPECT_EQ(test, subscriber_num_args.ret_subscriber_num, 1);
 }
 
@@ -45,19 +43,17 @@ void test_case_get_subscriber_num_many(struct kunit * test)
     bool is_take_sub = (i % 2) == 0;
 
     union ioctl_new_shm_args new_shm_args;
-    int ret1 = new_shm_addr(subscriber_pid, 1024, &new_shm_args);
+    new_shm_addr(subscriber_pid, 1024, &new_shm_args);
 
     union ioctl_subscriber_args subscriber_args;
-    int ret2 = subscriber_add(
+    subscriber_add(
       topic_name, node_name, qos_depth, qos_is_transient_local, subscriber_pid, is_take_sub,
       &subscriber_args);
 
     union ioctl_get_subscriber_num_args subscriber_num_args;
-    int ret3 = get_subscriber_num(topic_name, &subscriber_num_args);
+    int ret = get_subscriber_num(topic_name, &subscriber_num_args);
 
-    KUNIT_EXPECT_EQ(test, ret1, 0);
-    KUNIT_EXPECT_EQ(test, ret2, 0);
-    KUNIT_EXPECT_EQ(test, ret3, 0);
+    KUNIT_EXPECT_EQ(test, ret, 0);
     KUNIT_EXPECT_EQ(test, subscriber_num_args.ret_subscriber_num, i);
   }
 }
@@ -73,29 +69,26 @@ void test_case_get_subscriber_num_different_topic(struct kunit * test)
   bool is_take_sub = false;
 
   union ioctl_new_shm_args new_shm_args;
-  int ret1 = new_shm_addr(subscriber_pid, 1024, &new_shm_args);
+  new_shm_addr(subscriber_pid, 1024, &new_shm_args);
 
   union ioctl_subscriber_args subscriber_args1;
-  int ret2 = subscriber_add(
+  subscriber_add(
     topic_name1, node_name, qos_depth, qos_is_transient_local, subscriber_pid, is_take_sub,
     &subscriber_args1);
 
   union ioctl_subscriber_args subscriber_args2;
-  int ret3 = subscriber_add(
+  subscriber_add(
     topic_name2, node_name, qos_depth, qos_is_transient_local, subscriber_pid, is_take_sub,
     &subscriber_args2);
 
   union ioctl_get_subscriber_num_args subscriber_num_args1;
-  int ret4 = get_subscriber_num(topic_name1, &subscriber_num_args1);
+  int ret1 = get_subscriber_num(topic_name1, &subscriber_num_args1);
 
   union ioctl_get_subscriber_num_args subscriber_num_args2;
-  int ret5 = get_subscriber_num(topic_name2, &subscriber_num_args2);
+  int ret2 = get_subscriber_num(topic_name2, &subscriber_num_args2);
 
   KUNIT_EXPECT_EQ(test, ret1, 0);
   KUNIT_EXPECT_EQ(test, ret2, 0);
-  KUNIT_EXPECT_EQ(test, ret3, 0);
-  KUNIT_EXPECT_EQ(test, ret4, 0);
-  KUNIT_EXPECT_EQ(test, ret5, 0);
   KUNIT_EXPECT_EQ(test, subscriber_num_args1.ret_subscriber_num, 1);
   KUNIT_EXPECT_EQ(test, subscriber_num_args2.ret_subscriber_num, 1);
 }
@@ -110,42 +103,38 @@ void test_case_get_subscriber_num_with_exit(struct kunit * test)
 
   pid_t subscriber_pid1 = 123;
   union ioctl_new_shm_args new_shm_args;
-  int ret1 = new_shm_addr(subscriber_pid1, 1024, &new_shm_args);
+  new_shm_addr(subscriber_pid1, 1024, &new_shm_args);
 
   union ioctl_subscriber_args subscriber_args;
-  int ret2 = subscriber_add(
+  subscriber_add(
     topic_name, node_name, qos_depth, qos_is_transient_local, subscriber_pid1, is_take_sub,
     &subscriber_args);
 
   pid_t subscriber_pid2 = 124;
   union ioctl_new_shm_args new_shm_args2;
-  int ret3 = new_shm_addr(subscriber_pid2, 1024, &new_shm_args2);
+  new_shm_addr(subscriber_pid2, 1024, &new_shm_args2);
 
   union ioctl_subscriber_args subscriber_args2;
-  int ret4 = subscriber_add(
+  subscriber_add(
     topic_name, node_name, qos_depth, qos_is_transient_local, subscriber_pid2, is_take_sub,
     &subscriber_args2);
 
   union ioctl_get_subscriber_num_args subscriber_num_args1;
-  int ret5 = get_subscriber_num(topic_name, &subscriber_num_args1);
+  int ret1 = get_subscriber_num(topic_name, &subscriber_num_args1);
 
   process_exit_cleanup(subscriber_pid1);
 
   union ioctl_get_subscriber_num_args subscriber_num_args2;
-  int ret6 = get_subscriber_num(topic_name, &subscriber_num_args2);
+  int ret2 = get_subscriber_num(topic_name, &subscriber_num_args2);
 
   process_exit_cleanup(subscriber_pid2);
 
   union ioctl_get_subscriber_num_args subscriber_num_args3;
-  int ret7 = get_subscriber_num(topic_name, &subscriber_num_args3);
+  int ret3 = get_subscriber_num(topic_name, &subscriber_num_args3);
 
   KUNIT_EXPECT_EQ(test, ret1, 0);
   KUNIT_EXPECT_EQ(test, ret2, 0);
   KUNIT_EXPECT_EQ(test, ret3, 0);
-  KUNIT_EXPECT_EQ(test, ret4, 0);
-  KUNIT_EXPECT_EQ(test, ret5, 0);
-  KUNIT_EXPECT_EQ(test, ret6, 0);
-  KUNIT_EXPECT_EQ(test, ret7, 0);
   KUNIT_EXPECT_EQ(test, subscriber_num_args1.ret_subscriber_num, 2);
   KUNIT_EXPECT_EQ(test, subscriber_num_args2.ret_subscriber_num, 1);
   KUNIT_EXPECT_EQ(test, subscriber_num_args3.ret_subscriber_num, 0);
@@ -160,13 +149,12 @@ void test_case_get_subscriber_num_no_subscriber(struct kunit * test)
   bool qos_is_transient_local = false;
 
   union ioctl_publisher_args publisher_args;
-  int ret1 = publisher_add(
+  publisher_add(
     topic_name, node_name, publisher_pid, qos_depth, qos_is_transient_local, &publisher_args);
 
   union ioctl_get_subscriber_num_args subscriber_num_args;
-  int ret2 = get_subscriber_num(topic_name, &subscriber_num_args);
+  int ret = get_subscriber_num(topic_name, &subscriber_num_args);
 
-  KUNIT_EXPECT_EQ(test, ret1, 0);
-  KUNIT_EXPECT_EQ(test, ret2, 0);
+  KUNIT_EXPECT_EQ(test, ret, 0);
   KUNIT_EXPECT_EQ(test, subscriber_num_args.ret_subscriber_num, 0);
 }
