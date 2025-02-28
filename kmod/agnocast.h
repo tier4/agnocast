70f1b0584e813a37dd57e240a9056af670352e88
Koichi Imai <45482193+Koichi98@users.noreply.github.com>
2025-02-28 15:11:56 +0900

    refactor: reorder subscriber_args member variables (#431)
    * reorder subscriber_args member variables
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    
    * fix kunit test
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    
    ---------
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- kmod/agnocast.h
+++ kmod/agnocast.h
@@ -21,9 +21,9 @@ union ioctl_subscriber_args {
   {
     const char * topic_name;
     const char * node_name;
+    pid_t subscriber_pid;
     uint32_t qos_depth;
     bool qos_is_transient_local;
-    pid_t subscriber_pid;
     bool is_take_sub;
   };
   struct
@@ -168,8 +168,8 @@ void agnocast_exit_kprobe(void);
 void agnocast_exit_device(void);
 
 int subscriber_add(
-  char * topic_name, char * node_name, uint32_t qos_depth, bool qos_is_transient_local,
-  const pid_t subscriber_pid, bool is_take_sub, union ioctl_subscriber_args * ioctl_ret);
+  char * topic_name, char * node_name, const pid_t subscriber_pid, uint32_t qos_depth,
+  bool qos_is_transient_local, bool is_take_sub, union ioctl_subscriber_args * ioctl_ret);
 
 int publisher_add(
   const char * topic_name, const char * node_name, const pid_t publisher_pid,
