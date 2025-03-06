f89586fe47b788e53548d8714de6c66dbb3ea7f7
Enigamict <atsuki.takata@tier4.jp>
2025-03-06 21:28:54 +0900

    test(kmod) Add test for increment_message_entry function
    Signed-off-by: Enigamict <atsuki.takata@tier4.jp>

--- agnocast_kmod/agnocast_kunit/agnocast_kunit_increment_rc.h
+++ agnocast_kmod/agnocast_kunit/agnocast_kunit_increment_rc.h
@@ -2,7 +2,6 @@
 #include <kunit/test.h>
 
 #define TEST_CASES_INCREMENT_RC \
-  KUNIT_CASE(test_case_increment_rc_sample0), KUNIT_CASE(test_case_increment_rc_sample1)
+  KUNIT_CASE(test_case_increment_rc)
 
-void test_case_increment_rc_sample0(struct kunit * test);
-void test_case_increment_rc_sample1(struct kunit * test);
+void test_case_increment_rc(struct kunit * test);
