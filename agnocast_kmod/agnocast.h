e0200437cded4386bba6de385d51f234babc9d2b
atsushi421 <atsushi.yano.2@tier4.jp>
2025-03-13 04:38:53 +0900

    fix

--- agnocast_kmod/agnocast.h
+++ agnocast_kmod/agnocast.h
@@ -229,7 +229,7 @@ void process_exit_cleanup(const pid_t pid);
 int get_proc_info_htable_size(void);
 bool is_in_proc_info_htable(const pid_t pid);
 int get_topic_entries_num(const char * topic_name);
-bool is_in_topic_entries(char * topic_name, int64_t entry_id);
+bool is_in_topic_entries(const char * topic_name, int64_t entry_id);
 int get_entry_rc(const char * topic_name, const int64_t entry_id, const topic_local_id_t pubsub_id);
 bool is_in_subscriber_htable(const char * topic_name, const topic_local_id_t subscriber_id);
 int get_publisher_num(const char * topic_name);
