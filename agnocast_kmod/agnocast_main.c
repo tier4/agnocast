3806e1cc1967530edb332a9a4cfb034bfc7a6f32
veqcc <ryuta.kambe@tier4.jp>
2025-03-12 13:28:44 +0900

    update
    Signed-off-by: veqcc <ryuta.kambe@tier4.jp>

--- agnocast_kmod/agnocast_main.c
+++ agnocast_kmod/agnocast_main.c
@@ -228,7 +228,7 @@ static int insert_subscriber_info(
     new_id, subscriber_pid, wrapper->key);
 
   // Check if the topic has any volatile publishers.
-  if (!is_take_sub && qos_is_transient_local) {
+  if (qos_is_transient_local) {
     struct publisher_info * pub_info;
     int bkt_pub_info;
     hash_for_each(wrapper->topic.pub_info_htable, bkt_pub_info, pub_info, node)
@@ -930,24 +930,32 @@ int subscriber_add(
   }
 
   ioctl_ret->ret_transient_local_num = 0;
-  if (is_take_sub || !qos_is_transient_local) {
+  if (!qos_is_transient_local) {
     return 0;
   }
 
-  // Return qos_depth messages in order from newest to oldest for transient local
+  int transient_local_num = 0;
   for (struct rb_node * node = rb_last(&wrapper->topic.entries); node; node = rb_prev(node)) {
-    if (qos_depth <= ioctl_ret->ret_transient_local_num) break;
+    if (qos_depth <= transient_local_num) break;
 
     struct entry_node * en = container_of(node, struct entry_node, node);
 
-    ret = increment_sub_rc(en, sub_info->id);
-    if (ret < 0) {
-      return ret;
+    if (is_take_sub) {
+      // Update latest_received_entry_id for take subscriber
+      sub_info->latest_received_entry_id = en->entry_id;
+    } else {
+      // Return qos_depth messages in order from newest to oldest for non-take subscriber
+      ret = increment_sub_rc(en, sub_info->id);
+      if (ret < 0) {
+        return ret;
+      }
+
+      ioctl_ret->ret_entry_ids[ioctl_ret->ret_transient_local_num] = en->entry_id;
+      ioctl_ret->ret_entry_addrs[ioctl_ret->ret_transient_local_num] = en->msg_virtual_address;
+      ioctl_ret->ret_transient_local_num++;
     }
 
-    ioctl_ret->ret_entry_ids[ioctl_ret->ret_transient_local_num] = en->entry_id;
-    ioctl_ret->ret_entry_addrs[ioctl_ret->ret_transient_local_num] = en->msg_virtual_address;
-    ioctl_ret->ret_transient_local_num++;
+    transient_local_num++;
   }
 
   return 0;
