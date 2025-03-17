6be13d1f93a5bc412b0e66792f80908f37c26aa1
veqcc <ryuta.kambe@tier4.jp>
2025-03-17 16:49:33 +0900

    fix
    Signed-off-by: veqcc <ryuta.kambe@tier4.jp>

--- src/agnocastlib/src/agnocast_callback_info.cpp
+++ src/agnocastlib/src/agnocast_callback_info.cpp
@@ -6,6 +6,7 @@ namespace agnocast
 std::mutex id2_callback_info_mtx;
 const int callback_map_bkt_cnt = 100;  // arbitrary size to prevent rehash
 std::unordered_map<uint32_t, CallbackInfo> id2_callback_info(callback_map_bkt_cnt);
+std::atomic<uint32_t> next_callback_info_id;
 std::atomic<bool> need_epoll_updates{false};
 
 }  // namespace agnocast
