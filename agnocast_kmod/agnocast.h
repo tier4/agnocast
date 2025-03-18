4758226cceba4434da480dc582a112292cb3f1c6
bdm-k <kokusyunn@gmail.com>
2025-03-18 18:12:56 +0900

    fix: pass string length along with its pointer

--- agnocast_kmod/agnocast.h
+++ agnocast_kmod/agnocast.h
@@ -16,12 +16,17 @@ struct publisher_shm_info
   uint64_t shm_addrs[MAX_PUBLISHER_NUM];
   uint64_t shm_sizes[MAX_PUBLISHER_NUM];
 };
+struct name_info
+{
+  const char * ptr;
+  uint64_t len;
+};
 
 union ioctl_subscriber_args {
   struct
   {
-    const char * topic_name;
-    const char * node_name;
+    struct name_info topic_name;
+    struct name_info node_name;
     pid_t subscriber_pid;
     uint32_t qos_depth;
     bool qos_is_transient_local;
@@ -40,8 +45,8 @@ union ioctl_subscriber_args {
 union ioctl_publisher_args {
   struct
   {
-    const char * topic_name;
-    const char * node_name;
+    struct name_info topic_name;
+    struct name_info node_name;
     pid_t publisher_pid;
     uint32_t qos_depth;
     bool qos_is_transient_local;
@@ -54,7 +59,7 @@ union ioctl_publisher_args {
 
 struct ioctl_update_entry_args
 {
-  const char * topic_name;
+  struct name_info topic_name;
   topic_local_id_t pubsub_id;
   int64_t entry_id;
 };
@@ -62,7 +67,7 @@ struct ioctl_update_entry_args
 union ioctl_receive_msg_args {
   struct
   {
-    const char * topic_name;
+    struct name_info topic_name;
     topic_local_id_t subscriber_id;
   };
   struct
@@ -77,7 +82,7 @@ union ioctl_receive_msg_args {
 union ioctl_publish_args {
   struct
   {
-    const char * topic_name;
+    struct name_info topic_name;
     topic_local_id_t publisher_id;
     uint64_t msg_virtual_address;
   };
@@ -94,7 +99,7 @@ union ioctl_publish_args {
 union ioctl_take_msg_args {
   struct
   {
-    const char * topic_name;
+    struct name_info topic_name;
     topic_local_id_t subscriber_id;
     bool allow_same_message;
   };
@@ -116,7 +121,7 @@ union ioctl_new_shm_args {
 };
 
 union ioctl_get_subscriber_num_args {
-  const char * topic_name;
+  struct name_info topic_name;
   uint32_t ret_subscriber_num;
 };
 
@@ -143,7 +148,7 @@ union ioctl_topic_list_args {
 union ioctl_node_info_args {
   struct
   {
-    const char * node_name;
+    struct name_info node_name;
     uint64_t topic_name_buffer_addr;
   };
   uint32_t ret_topic_num;
@@ -159,7 +164,7 @@ struct topic_info_ret
 union ioctl_topic_info_args {
   struct
   {
-    const char * topic_name;
+    struct name_info topic_name;
     uint64_t topic_info_ret_buffer_addr;
   };
   uint32_t ret_topic_info_ret_num;
