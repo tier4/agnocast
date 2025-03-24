471e2829e56bce2722135ebcb63cb4996311a574
atsushi421 <atsushi.yano.2@tier4.jp>
2025-03-24 17:06:27 +0900

    fix

--- src/agnocastlib/test/integration/test_agnocast_heaphook.cpp
+++ src/agnocastlib/test/integration/test_agnocast_heaphook.cpp
@@ -1,13 +1,10 @@
 #include <gtest/gtest.h>
 
 // Mock
-namespace agnocast
-{
-void * initialize_agnocast(const uint64_t shm_size)
+extern "C" void * initialize_agnocast(const uint64_t shm_size)
 {
   return nullptr;
 }
-}  // namespace agnocast
 
 TEST(agnocast_heaphook, dummy)
 {
