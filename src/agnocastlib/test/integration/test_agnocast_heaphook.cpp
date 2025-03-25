8a5600784e3e4e1b5957f2b7c7c5d3c0668b1661
atsushi421 <atsushi.yano.2@tier4.jp>
2025-03-25 09:59:32 +0900

    fix

--- src/agnocastlib/test/integration/test_agnocast_heaphook.cpp
+++ src/agnocastlib/test/integration/test_agnocast_heaphook.cpp
@@ -1,11 +1,5 @@
 #include <gtest/gtest.h>
 
-// Mock
-extern "C" void * initialize_agnocast(const uint64_t shm_size)
-{
-  return nullptr;
-}
-
 TEST(agnocast_heaphook, dummy)
 {
   // Dummy test to avoid "No tests found in test_agnocast_heaphook.cpp" error
