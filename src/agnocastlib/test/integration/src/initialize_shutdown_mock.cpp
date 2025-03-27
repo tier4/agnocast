c07657dc63e86fca39e8d80f3b68fc49d9eafa0a
Koichi Imai <45482193+Koichi98@users.noreply.github.com>
2025-03-27 17:07:42 +0900

    fix(test): suppress warning for unused variable (#571)
    suppress warning
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- src/agnocastlib/test/integration/src/initialize_shutdown_mock.cpp
+++ src/agnocastlib/test/integration/src/initialize_shutdown_mock.cpp
@@ -13,6 +13,8 @@ int shm_fd;
 namespace agnocast
 {
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 extern "C" void * initialize_agnocast(
   const uint64_t shm_size, const unsigned char * heaphook_version_ptr,
   const size_t heaphook_version_str_len)
@@ -41,6 +43,7 @@ extern "C" void * initialize_agnocast(
 
   return ret;
 }
+#pragma GCC diagnostic pop
 
 void shutdown_agnocast()
 {
