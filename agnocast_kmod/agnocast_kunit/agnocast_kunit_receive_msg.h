65641ffe11642ba01d58f7aeecb6d0c43ad40edd
Koichi Imai <45482193+Koichi98@users.noreply.github.com>
2025-03-27 17:21:43 +0900

    feat(kmod): add transient local kunit test for receive_msg (#568)
    * add transient local test for receive_msg
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    
    * fix error and header file
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    
    * fix
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    
    ---------
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- agnocast_kmod/agnocast_kunit/agnocast_kunit_receive_msg.h
+++ agnocast_kmod/agnocast_kunit/agnocast_kunit_receive_msg.h
@@ -14,6 +14,14 @@
       test_case_receive_msg_sub_qos_depth_smaller_than_pub_qos_depth_smaller_than_publish_num),     \
     KUNIT_CASE(                                                                                     \
       test_case_receive_msg_publish_num_and_sub_qos_depth_and_pub_qos_depth_are_all_max_qos_depth), \
+    KUNIT_CASE(                                                                                     \
+      test_case_receive_msg_transient_local_sub_qos_and_pub_qos_and_publish_num_are_all_equal),     \
+    KUNIT_CASE(                                                                                     \
+      test_case_receive_msg_transient_local_sub_qos_smaller_than_pub_qos_smaller_than_publish_num), \
+    KUNIT_CASE(                                                                                     \
+      test_case_receive_msg_transient_local_sub_qos_smaller_than_publish_num_smaller_than_pub_qos), \
+    KUNIT_CASE(                                                                                     \
+      test_case_receive_msg_transient_local_publish_num_smaller_than_sub_qos_smaller_than_pub_qos), \
     KUNIT_CASE(test_case_receive_msg_one_new_pub),                                                  \
     KUNIT_CASE(test_case_receive_msg_pubsub_in_same_process),                                       \
     KUNIT_CASE(test_case_receive_msg_2pub_in_same_process),                                         \
@@ -32,6 +40,14 @@ void test_case_receive_msg_publish_num_smaller_than_sub_qos_depth_smaller_than_p
 void test_case_receive_msg_sub_qos_depth_smaller_than_pub_qos_depth_smaller_than_publish_num(
   struct kunit * test);
 void test_case_receive_msg_publish_num_and_sub_qos_depth_and_pub_qos_depth_are_all_max_qos_depth(
+  struct kunit * test);
+void test_case_receive_msg_transient_local_sub_qos_and_pub_qos_and_publish_num_are_all_equal(
+  struct kunit * test);
+void test_case_receive_msg_transient_local_sub_qos_smaller_than_pub_qos_smaller_than_publish_num(
+  struct kunit * test);
+void test_case_receive_msg_transient_local_sub_qos_smaller_than_publish_num_smaller_than_pub_qos(
+  struct kunit * test);
+void test_case_receive_msg_transient_local_publish_num_smaller_than_sub_qos_smaller_than_pub_qos(
   struct kunit * test);
 void test_case_receive_msg_one_new_pub(struct kunit * test);
 void test_case_receive_msg_pubsub_in_same_process(struct kunit * test);
