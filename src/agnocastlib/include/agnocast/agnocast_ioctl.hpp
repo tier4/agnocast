153c66b0776f20df8f65e725c750b9e6288f182e
Koichi Imai <koichi.imai.2@tier4.jp>
2025-04-07 15:06:29 +0900

    clang-format
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- src/agnocastlib/include/agnocast/agnocast_ioctl.hpp
+++ src/agnocastlib/include/agnocast/agnocast_ioctl.hpp
@@ -8,11 +8,11 @@
 namespace agnocast
 {
 
-#define MAX_PUBLISHER_NUM 4      // Maximum number of publishers per topic
-#define MAX_SUBSCRIBER_NUM 16    // Maximum number of subscribers per topic
-#define MAX_QOS_DEPTH 10         // Maximum QoS depth for each publisher/subscriber
-#define MAX_RELEASE_NUM 3        // Maximum number of entries that can be released at one ioctl
-#define VERSION_BUFFER_LEN 32    // Maximum size of version number represented as a string
+#define MAX_PUBLISHER_NUM 4        // Maximum number of publishers per topic
+#define MAX_SUBSCRIBER_NUM 16      // Maximum number of subscribers per topic
+#define MAX_QOS_DEPTH 10           // Maximum QoS depth for each publisher/subscriber
+#define MAX_RELEASE_NUM 3          // Maximum number of entries that can be released at one ioctl
+#define VERSION_BUFFER_LEN 32      // Maximum size of version number represented as a string
 #define MAX_UNLINK_PROCESS_NUM 16  // Maximum number of pids of exited process for unlink daemon
 
 using topic_local_id_t = int32_t;
