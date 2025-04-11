890296b7a0a5070ce07c6db9feecfe01cdf39a8a
Koichi Imai <45482193+Koichi98@users.noreply.github.com>
2025-04-11 15:31:06 +0900

    refactor(kmod): reorder ioctl command (#615)
    reorder ioctl command
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- agnocast_kmod/agnocast.h
+++ agnocast_kmod/agnocast.h
@@ -24,6 +24,20 @@ struct name_info
   uint64_t len;
 };
 
+struct ioctl_get_version_args
+{
+  char ret_version[VERSION_BUFFER_LEN];
+};
+
+union ioctl_add_process_args {
+  uint64_t shm_size;
+  struct
+  {
+    uint64_t ret_addr;
+    bool ret_unlink_daemon_exist;
+  };
+};
+
 union ioctl_add_subscriber_args {
   struct
   {
@@ -107,20 +121,6 @@ union ioctl_take_msg_args {
   };
 };
 
-union ioctl_add_process_args {
-  uint64_t shm_size;
-  struct
-  {
-    uint64_t ret_addr;
-    bool ret_unlink_daemon_exist;
-  };
-};
-
-struct ioctl_get_version_args
-{
-  char ret_version[VERSION_BUFFER_LEN];
-};
-
 union ioctl_get_subscriber_num_args {
   struct name_info topic_name;
   uint32_t ret_subscriber_num;
@@ -132,15 +132,15 @@ struct ioctl_get_exit_process_args
   pid_t ret_pid;
 };
 
-#define AGNOCAST_ADD_SUBSCRIBER_CMD _IOWR(0xA6, 1, union ioctl_add_subscriber_args)
-#define AGNOCAST_ADD_PUBLISHER_CMD _IOWR(0xA6, 2, union ioctl_add_publisher_args)
-#define AGNOCAST_INCREMENT_RC_CMD _IOW(0xA6, 3, struct ioctl_update_entry_args)
-#define AGNOCAST_DECREMENT_RC_CMD _IOW(0xA6, 4, struct ioctl_update_entry_args)
-#define AGNOCAST_RECEIVE_MSG_CMD _IOWR(0xA6, 5, union ioctl_receive_msg_args)
-#define AGNOCAST_PUBLISH_MSG_CMD _IOWR(0xA6, 6, union ioctl_publish_msg_args)
-#define AGNOCAST_TAKE_MSG_CMD _IOWR(0xA6, 7, union ioctl_take_msg_args)
-#define AGNOCAST_ADD_PROCESS_CMD _IOWR(0xA6, 8, union ioctl_add_process_args)
-#define AGNOCAST_GET_VERSION_CMD _IOR(0xA6, 9, struct ioctl_get_version_args)
+#define AGNOCAST_GET_VERSION_CMD _IOR(0xA6, 1, struct ioctl_get_version_args)
+#define AGNOCAST_ADD_PROCESS_CMD _IOWR(0xA6, 2, union ioctl_add_process_args)
+#define AGNOCAST_ADD_SUBSCRIBER_CMD _IOWR(0xA6, 3, union ioctl_add_subscriber_args)
+#define AGNOCAST_ADD_PUBLISHER_CMD _IOWR(0xA6, 4, union ioctl_add_publisher_args)
+#define AGNOCAST_INCREMENT_RC_CMD _IOW(0xA6, 5, struct ioctl_update_entry_args)
+#define AGNOCAST_DECREMENT_RC_CMD _IOW(0xA6, 6, struct ioctl_update_entry_args)
+#define AGNOCAST_PUBLISH_MSG_CMD _IOWR(0xA6, 7, union ioctl_publish_msg_args)
+#define AGNOCAST_RECEIVE_MSG_CMD _IOWR(0xA6, 8, union ioctl_receive_msg_args)
+#define AGNOCAST_TAKE_MSG_CMD _IOWR(0xA6, 9, union ioctl_take_msg_args)
 #define AGNOCAST_GET_SUBSCRIBER_NUM_CMD _IOWR(0xA6, 10, union ioctl_get_subscriber_num_args)
 #define AGNOCAST_GET_EXIT_PROCESS_CMD _IOR(0xA6, 11, struct ioctl_get_exit_process_args)
 
