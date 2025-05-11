4bf1e9bbfd7d3e40f7540e16ddb2f11e2b73167e
koichiimai <kotty.0704@gmail.com>
2025-05-11 13:23:38 +0900

    lock for atomically obtain shmem info via ioctl and do mmap
    Signed-off-by: koichiimai <kotty.0704@gmail.com>

--- src/agnocastlib/src/agnocast.cpp
+++ src/agnocastlib/src/agnocast.cpp
@@ -16,7 +16,8 @@ namespace agnocast
 int agnocast_fd = -1;
 std::vector<int> shm_fds;
 std::mutex shm_fds_mtx;
-
+// To obtain shm info for the publisher process via ioctl(RECEIVE_MSG) and perform mmap atomically.
+std::mutex mmap_mtx;
 void poll_for_unlink()
 {
   if (setsid() == -1) {
