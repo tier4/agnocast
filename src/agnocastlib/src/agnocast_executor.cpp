1fa507f8a0a0eada9768fda6860eadee42845b4d
Koichi Imai <45482193+Koichi98@users.noreply.github.com>
2025-05-12 11:57:05 +0900

    fix: lock for atomically obtain shmem info via ioctl and do mmap (#639)
    * lock for atomically obtain shmem info via ioctl and do mmap
    
    Signed-off-by: koichiimai <kotty.0704@gmail.com>
    
    * fix comment
    
    Signed-off-by: koichiimai <kotty.0704@gmail.com>
    
    * add more details in comment
    
    Signed-off-by: koichiimai <kotty.0704@gmail.com>
    
    ---------
    
    Signed-off-by: koichiimai <kotty.0704@gmail.com>

--- src/agnocastlib/src/agnocast_executor.cpp
+++ src/agnocastlib/src/agnocast_executor.cpp
@@ -29,18 +29,23 @@ void AgnocastExecutor::receive_message(
   union ioctl_receive_msg_args receive_args = {};
   receive_args.topic_name = {callback_info.topic_name.c_str(), callback_info.topic_name.size()};
   receive_args.subscriber_id = callback_info.subscriber_id;
-  if (ioctl(agnocast_fd, AGNOCAST_RECEIVE_MSG_CMD, &receive_args) < 0) {
-    RCLCPP_ERROR(logger, "AGNOCAST_RECEIVE_MSG_CMD failed: %s", strerror(errno));
-    close(agnocast_fd);
-    exit(EXIT_FAILURE);
-  }
 
-  // Map the shared memory region with read permissions whenever a new publisher is discovered.
-  for (uint32_t i = 0; i < receive_args.ret_pub_shm_info.publisher_num; i++) {
-    const pid_t pid = receive_args.ret_pub_shm_info.publisher_pids[i];
-    const uint64_t addr = receive_args.ret_pub_shm_info.shm_addrs[i];
-    const uint64_t size = receive_args.ret_pub_shm_info.shm_sizes[i];
-    map_read_only_area(pid, addr, size);
+  {
+    std::lock_guard<std::mutex> lock(mmap_mtx);
+
+    if (ioctl(agnocast_fd, AGNOCAST_RECEIVE_MSG_CMD, &receive_args) < 0) {
+      RCLCPP_ERROR(logger, "AGNOCAST_RECEIVE_MSG_CMD failed: %s", strerror(errno));
+      close(agnocast_fd);
+      exit(EXIT_FAILURE);
+    }
+
+    // Map the shared memory region with read permissions whenever a new publisher is discovered.
+    for (uint32_t i = 0; i < receive_args.ret_pub_shm_info.publisher_num; i++) {
+      const pid_t pid = receive_args.ret_pub_shm_info.publisher_pids[i];
+      const uint64_t addr = receive_args.ret_pub_shm_info.shm_addrs[i];
+      const uint64_t size = receive_args.ret_pub_shm_info.shm_sizes[i];
+      map_read_only_area(pid, addr, size);
+    }
   }
 
   // older messages first
