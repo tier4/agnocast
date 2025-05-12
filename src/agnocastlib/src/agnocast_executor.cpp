202ed2ba4b0ebe66a44c810d34f1028e29cf31a6
atsushi421 <atsushi.yano.2@tier4.jp>
2025-05-12 12:05:08 +0900

    fix comment

--- src/agnocastlib/src/agnocast_executor.cpp
+++ src/agnocastlib/src/agnocast_executor.cpp
@@ -181,7 +181,8 @@ bool AgnocastExecutor::get_next_ready_agnocast_executable(AgnocastExecutable & a
 
   for (auto it = ready_agnocast_executables_.begin(); it != ready_agnocast_executables_.end();
        ++it) {
-    // If the executor->add_node() is not called for this callback_group, it will be nullptr.
+    // If the executor->add_node() is not called for the node that has this callback_group,
+    // get_node_by_group() returns nullptr.
     if (
       rclcpp::Executor::get_node_by_group(
         rclcpp::Executor::weak_groups_to_nodes_, it->callback_group) == nullptr) {
