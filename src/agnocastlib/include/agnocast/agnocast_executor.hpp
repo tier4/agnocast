3066b188901cd3a827740d4974f32b52f7af4166
sykwer <sykwer@gmail.com>
2025-07-09 20:53:49 +0900

    fix
    Signed-off-by: sykwer <sykwer@gmail.com>

--- src/agnocastlib/include/agnocast/agnocast_executor.hpp
+++ src/agnocastlib/include/agnocast/agnocast_executor.hpp
@@ -26,7 +26,7 @@ class AgnocastExecutor : public rclcpp::Executor
 
   void wait_and_handle_epoll_event(const int timeout_ms);
   bool get_next_ready_agnocast_executable(AgnocastExecutable & agnocast_executable);
-  virtual void validate_callback_group(const rclcpp::CallbackGroup::SharedPtr & group) const = 0;
+  virtual bool validate_callback_group(const rclcpp::CallbackGroup::SharedPtr & group) const = 0;
 
 protected:
   int epoll_fd_;
