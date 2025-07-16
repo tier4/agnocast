3e0eff57993cc36b77e08dfa51a0a2ea72f1ceba
Takahiro Ishikawa-Aso <sykwer@gmail.com>
2025-07-17 06:47:01 +0900

    fix(agnocastlib): use override keyword (#662)
    Signed-off-by: sykwer <sykwer@gmail.com>

--- src/agnocastlib/include/agnocast/agnocast_executor.hpp
+++ src/agnocastlib/include/agnocast/agnocast_executor.hpp
@@ -49,7 +49,7 @@ public:
 
   void add_node(rclcpp::Node::SharedPtr node, bool notify = true) override;
 
-  virtual void spin() = 0;
+  virtual void spin() override = 0;
 };
 
 }  // namespace agnocast
