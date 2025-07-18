0a0a6e6d5ae3eb9de72c2eba067aed4b16a4e5b1
sykwer <sykwer@gmail.com>
2025-07-19 01:42:15 +0900

    feat(agnocastlib): add cie skelton
    Signed-off-by: sykwer <sykwer@gmail.com>

---
+++ src/agnocastlib/src/agnocast_callback_isolated_executor.cpp
@@ -0,0 +1,86 @@
+#include <agnocast/agnocast.hpp>
+#include <agnocast/agnocast_callback_isolated_executor.hpp>
+#include <agnocast/agnocast_single_threaded_executor.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+namespace agnocast
+{
+
+CallbackIsolatedAgnocastExecutor::CallbackIsolatedAgnocastExecutor(
+  const rclcpp::ExecutorOptions & options)
+: rclcpp::Executor(options)
+{
+  // TODO: add support for CARET tracepoint
+}
+
+void CallbackIsolatedAgnocastExecutor::spin()
+{
+  if (spinning.exchange(true)) {
+    RCLCPP_ERROR(logger, "spin() called while already spinning");
+    close(agnocast_fd);
+    exit(EXIT_FAILURE);
+  }
+}
+
+// implmenet methods in .hpp
+void CallbackIsolatedAgnocastExecutor::add_callback_group(
+  rclcpp::CallbackGroup::SharedPtr group_ptr,
+  rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_ptr, bool notify)
+{
+  (void)group_ptr;
+  (void)node_ptr;
+  (void)notify;
+}
+
+std::vector<rclcpp::CallbackGroup::WeakPtr>
+CallbackIsolatedAgnocastExecutor::get_all_callback_groups()
+{
+  return std::vector<rclcpp::CallbackGroup::WeakPtr>();
+}
+
+std::vector<rclcpp::CallbackGroup::WeakPtr>
+CallbackIsolatedAgnocastExecutor::get_manually_added_callback_groups()
+{
+  return std::vector<rclcpp::CallbackGroup::WeakPtr>();
+}
+
+std::vector<rclcpp::CallbackGroup::WeakPtr>
+CallbackIsolatedAgnocastExecutor::get_automatically_added_callback_groups_from_nodes()
+{
+  return std::vector<rclcpp::CallbackGroup::WeakPtr>();
+}
+
+void CallbackIsolatedAgnocastExecutor::remove_callback_group(
+  rclcpp::CallbackGroup::SharedPtr group_ptr, bool notify)
+{
+  (void)group_ptr;
+  (void)notify;
+}
+
+void CallbackIsolatedAgnocastExecutor::add_node(
+  rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_ptr, bool notify)
+{
+  (void)node_ptr;
+  (void)notify;
+}
+
+void CallbackIsolatedAgnocastExecutor::add_node(rclcpp::Node::SharedPtr node_ptr, bool notify)
+{
+  (void)node_ptr;
+  (void)notify;
+}
+
+void CallbackIsolatedAgnocastExecutor::remove_node(
+  rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_ptr, bool notify)
+{
+  (void)node_ptr;
+  (void)notify;
+}
+
+void CallbackIsolatedAgnocastExecutor::remove_node(rclcpp::Node::SharedPtr node_ptr, bool notify)
+{
+  (void)node_ptr;
+  (void)notify;
+}
+
+}  // namespace agnocast
