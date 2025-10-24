c520c8beaf70ddca5c25bdde8fac85d85e484c2c
kobayu858 <yutaro.kobayashi.2@tier4.jp>
2025-10-24 14:00:37 +0900

    feat: gen a2r sharelib
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

--- src/agnocastlib/include/agnocast/agnocast_bridge_plugin_api.hpp
+++ src/agnocastlib/include/agnocast/agnocast_bridge_plugin_api.hpp
@@ -1,8 +1,13 @@
-// bridge_plugin_api.hpp
 #pragma once
+
+#include "agnocast/agnocast_subscription.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 extern "C" rclcpp::SubscriptionBase::SharedPtr create_r2a_bridge(
   rclcpp::Node::SharedPtr node, const std::string & topic_name, const rclcpp::QoS & qos);
 
+extern "C" std::shared_ptr<agnocast::SubscriptionBase> create_a2r_bridge(
+  rclcpp::Node::SharedPtr node, const std::string & topic_name, const rclcpp::QoS & qos);
+
 using CreateR2ABridgeFunc = decltype(&create_r2a_bridge);
+using CreateA2RBridgeFunc = decltype(&create_a2r_bridge);
