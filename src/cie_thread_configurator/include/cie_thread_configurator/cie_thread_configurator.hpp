c542226c5cbd316bfdc2093274353d6569131331
atsushi yano <55824710+atsushi421@users.noreply.github.com>
2025-11-11 08:14:27 +0900

    feat: port thread configurator from callback_isolated_executor repo (#720)
    * feat: port cie_thread_configurator
    
    * fix
    
    * fix: cppcheck

---
+++ src/cie_thread_configurator/include/cie_thread_configurator/cie_thread_configurator.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "cie_config_msgs/msg/callback_group_info.hpp"
+
+#include <map>
+#include <memory>
+#include <string>
+
+namespace cie_thread_configurator
+{
+
+std::string create_callback_group_id(
+  rclcpp::CallbackGroup::SharedPtr group, rclcpp::Node::SharedPtr node);
+
+std::string create_callback_group_id(
+  rclcpp::CallbackGroup::SharedPtr group,
+  rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node);
+
+// Caution: Do not call in parallel
+// Caution: Must be called after rclcpp::init() called
+rclcpp::Publisher<cie_config_msgs::msg::CallbackGroupInfo>::SharedPtr create_client_publisher();
+
+// `publisher` is assumed to be the return value of create_client_publisher()
+void publish_callback_group_info(
+  const rclcpp::Publisher<cie_config_msgs::msg::CallbackGroupInfo>::SharedPtr & publisher,
+  int64_t tid, const std::string & callback_group_id);
+
+// Get hardware information from lscpu command
+std::map<std::string, std::string> get_hardware_info();
+
+}  // namespace cie_thread_configurator
