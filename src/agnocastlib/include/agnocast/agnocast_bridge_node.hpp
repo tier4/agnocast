2b2a3bbdd4aa4355c86b23ea96526e108ad09d83
kobayu858 <yutaro.kobayashi.2@tier4.jp>
2025-11-19 17:02:06 +0900

    refactor: separete
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

--- src/agnocastlib/include/agnocast/agnocast_bridge_node.hpp
+++ src/agnocastlib/include/agnocast/agnocast_bridge_node.hpp
@@ -17,28 +17,6 @@
 namespace agnocast
 {
 
-inline QoSFlat flatten_qos(const rclcpp::QoS & qos)
-{
-  QoSFlat out{};
-  const auto & rmw_qos = qos.get_rmw_qos_profile();
-  out.depth = rmw_qos.depth;
-  out.history = (rmw_qos.history == RMW_QOS_POLICY_HISTORY_KEEP_ALL) ? 1 : 0;
-  out.reliability = (rmw_qos.reliability == RMW_QOS_POLICY_RELIABILITY_RELIABLE) ? 1 : 2;
-  out.durability = (rmw_qos.durability == RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL) ? 1 : 2;
-  return out;
-}
-
-inline void safe_strncpy(char * dest, const char * src, size_t dest_size)
-{
-  if (dest_size == 0) return;
-  if (src == nullptr) {
-    dest[0] = '\0';
-    return;
-  }
-  std::strncpy(dest, src, dest_size - 1);
-  dest[dest_size - 1] = '\0';
-}
-
 template <typename MessageT>
 class BridgeNode : public rclcpp::Node
 {
--- src/agnocastlib/include/agnocast/agnocast_bridge_utils.hpp
+++ src/agnocastlib/include/agnocast/agnocast_bridge_utils.hpp
@@ -1,8 +1,13 @@
+#pragma once
+
 #include "agnocast/agnocast_mq.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 namespace agnocast
 {
+
+QoSFlat flatten_qos(const rclcpp::QoS & qos);
+void safe_strncpy(char * dest, const char * src, size_t dest_size);
 
 using BridgeFn = std::shared_ptr<rclcpp::Node> (*)(const BridgeArgs &);
 
