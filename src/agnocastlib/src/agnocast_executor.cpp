bfe00c5a69cf376a599ebc2b4afb175aeb408b0c
Takahiro Ishikawa-Aso <sykwer@gmail.com>
2025-11-25 09:48:52 +0900

    fix(agnocastlib): extract receive_message as independent function (#738)
    Signed-off-by: sykwer <sykwer@gmail.com>

--- src/agnocastlib/src/agnocast_executor.cpp
+++ src/agnocastlib/src/agnocast_executor.cpp
@@ -22,59 +22,6 @@ AgnocastExecutor::~AgnocastExecutor()
   close(epoll_fd_);
 }
 
-void AgnocastExecutor::receive_message(
-  [[maybe_unused]] const uint32_t callback_info_id,  // for CARET
-  const CallbackInfo & callback_info)
-{
-  union ioctl_receive_msg_args receive_args = {};
-  receive_args.topic_name = {callback_info.topic_name.c_str(), callback_info.topic_name.size()};
-  receive_args.subscriber_id = callback_info.subscriber_id;
-
-  {
-    std::lock_guard<std::mutex> lock(mmap_mtx);
-
-    if (ioctl(agnocast_fd, AGNOCAST_RECEIVE_MSG_CMD, &receive_args) < 0) {
-      RCLCPP_ERROR(logger, "AGNOCAST_RECEIVE_MSG_CMD failed: %s", strerror(errno));
-      close(agnocast_fd);
-      exit(EXIT_FAILURE);
-    }
-
-    // Map the shared memory region with read permissions whenever a new publisher is discovered.
-    for (uint32_t i = 0; i < receive_args.ret_pub_shm_info.publisher_num; i++) {
-      const pid_t pid = receive_args.ret_pub_shm_info.publisher_pids[i];
-      const uint64_t addr = receive_args.ret_pub_shm_info.shm_addrs[i];
-      const uint64_t size = receive_args.ret_pub_shm_info.shm_sizes[i];
-      map_read_only_area(pid, addr, size);
-    }
-  }
-
-  // older messages first
-  for (int32_t i = static_cast<int32_t>(receive_args.ret_entry_num) - 1; i >= 0; i--) {
-    const std::shared_ptr<std::function<void()>> callable =
-      std::make_shared<std::function<void()>>([callback_info, receive_args, i]() {
-        auto typed_msg = callback_info.message_creator(
-          reinterpret_cast<void *>(receive_args.ret_entry_addrs[i]), callback_info.topic_name,
-          callback_info.subscriber_id, receive_args.ret_entry_ids[i]);
-        callback_info.callback(std::move(*typed_msg));
-      });
-
-    {
-      constexpr uint8_t PID_SHIFT_BITS = 32;
-      uint64_t pid_callback_info_id =
-        (static_cast<uint64_t>(my_pid_) << PID_SHIFT_BITS) | callback_info_id;
-      TRACEPOINT(
-        agnocast_create_callable, static_cast<const void *>(callable.get()),
-        receive_args.ret_entry_ids[i], pid_callback_info_id);
-    }
-
-    {
-      std::lock_guard<std::mutex> ready_lock{ready_agnocast_executables_mutex_};
-      ready_agnocast_executables_.emplace_back(
-        AgnocastExecutable{callable, callback_info.callback_group});
-    }
-  }
-}
-
 void AgnocastExecutor::prepare_epoll()
 {
   std::lock_guard<std::mutex> lock(id2_callback_info_mtx);
@@ -102,7 +49,9 @@ void AgnocastExecutor::prepare_epoll()
     }
 
     if (callback_info.is_transient_local) {
-      receive_message(callback_info_id, callback_info);
+      agnocast::receive_message(
+        callback_info_id, my_pid_, callback_info, ready_agnocast_executables_mutex_,
+        ready_agnocast_executables_);
     }
 
     callback_info.need_epoll_update = false;
@@ -183,7 +132,9 @@ void AgnocastExecutor::wait_and_handle_epoll_event(const int timeout_ms)
     return;
   }
 
-  receive_message(callback_info_id, callback_info);
+  agnocast::receive_message(
+    callback_info_id, my_pid_, callback_info, ready_agnocast_executables_mutex_,
+    ready_agnocast_executables_);
 }
 
 bool AgnocastExecutor::get_next_ready_agnocast_executable(AgnocastExecutable & agnocast_executable)
