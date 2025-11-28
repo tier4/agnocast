458855981b066bfbbc313bf0046ba44825728f34
Koichi Imai <koichi.imai.2@tier4.jp>
2025-11-28 16:59:28 +0900

    minor fix
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- src/agnocastlib/include/agnocast/agnocast_timer.hpp
+++ src/agnocastlib/include/agnocast/agnocast_timer.hpp
@@ -8,18 +8,8 @@ namespace agnocast
 class AgnocastTimer
 {
 public:
-  /**
-   * @brief Construct a new Agnocast Timer object
-   *
-   * @param period Timer period in nanoseconds
-   * @param callback Function to call when timer expires
-   * @throws std::runtime_error if timer creation fails
-   */
   AgnocastTimer(std::chrono::nanoseconds period, std::function<void()> callback);
 
-  /**
-   * @brief Destroy the Agnocast Timer object
-   */
   ~AgnocastTimer();
 
   // Disable copy and move
@@ -28,36 +18,14 @@ public:
   AgnocastTimer(AgnocastTimer &&) = delete;
   AgnocastTimer & operator=(AgnocastTimer &&) = delete;
 
-  /**
-   * @brief Get the file descriptor for this timer
-   *
-   * @return int Timer file descriptor
-   */
   int get_fd() const;
 
-  /**
-   * @brief Handle a timer event (read expiration count and execute callback)
-   */
   void handle_event();
 
-  /**
-   * @brief Cancel the timer
-   */
   void cancel();
 
-  /**
-   * @brief Check if timer is canceled
-   *
-   * @return true if canceled
-   */
   bool is_canceled() const;
 
-  /**
-   * @brief Reset the timer with a new period
-   *
-   * @param period New timer period in nanoseconds
-   * @throws std::runtime_error if timer update fails
-   */
   void reset(std::chrono::nanoseconds period);
 
 private:
