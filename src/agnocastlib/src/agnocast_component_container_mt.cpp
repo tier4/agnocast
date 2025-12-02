38cadf62a288f104f3ebf66c09ab2dea975a361c
atsushi421 <atsushi.yano.2@tier4.jp>
2025-12-02 15:18:06 +0900

    perf: change default values of next_exec in MultiThreadedAgnocastExecutor
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

--- src/agnocastlib/src/agnocast_component_container_mt.cpp
+++ src/agnocastlib/src/agnocast_component_container_mt.cpp
@@ -23,9 +23,9 @@ int main(int argc, char * argv[])
       node->get_parameter_or("number_of_agnocast_threads", 0);
     const bool yield_before_execute = node->get_parameter_or("yield_before_execute", false);
     const nanoseconds ros2_next_exec_timeout_ns =
-      nanoseconds(node->get_parameter_or("ros2_next_exec_timeout_ms", 10) * 1000 * 1000);
+      nanoseconds(node->get_parameter_or("ros2_next_exec_timeout_ns", -1));
     const int agnocast_next_exec_timeout_ms =
-      node->get_parameter_or("agnocast_next_exec_timeout_ms", 10);
+      node->get_parameter_or("agnocast_next_exec_timeout_ms", 50);
 
     auto executor = std::make_shared<agnocast::MultiThreadedAgnocastExecutor>(
       rclcpp::ExecutorOptions{}, number_of_ros2_threads, number_of_agnocast_threads,
