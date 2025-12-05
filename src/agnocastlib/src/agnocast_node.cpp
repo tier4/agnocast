9f1be3bc28434e10c514ff80626b8d30ed66b224
Koichi Imai <koichi.imai.2@tier4.jp>
2025-12-05 20:20:59 +0900

    node base interface
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

---
+++ src/agnocastlib/src/agnocast_node.cpp
@@ -0,0 +1,29 @@
+#include "agnocast/agnocast_node.hpp"
+
+#include "agnocast/agnocast_context.hpp"
+
+namespace agnocast
+{
+
+Node::Node(const std::string & node_name, const rclcpp::NodeOptions & options)
+{
+  initialize_node(node_name, "", options.context());
+}
+
+Node::Node(
+  const std::string & node_name, const std::string & namespace_,
+  const rclcpp::NodeOptions & options)
+{
+  initialize_node(node_name, namespace_, options.context());
+}
+
+void Node::initialize_node(
+  const std::string & node_name, const std::string & ns, rclcpp::Context::SharedPtr context)
+{
+  node_base_ = std::make_shared<node_interfaces::NodeBase>(node_name, ns, std::move(context));
+  logger_ = rclcpp::get_logger(node_base_->get_name());
+
+  // TODO(Koichi98): Initialization of NodeTopicsInterface, NodeParametersInterface, etc.
+}
+
+}  // namespace agnocast
