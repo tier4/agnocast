90999968aa6e3ca15470923f09b3b90d104a3390
Yutaro Kobayashi <129580202+kobayu858@users.noreply.github.com>
2025-12-09 17:52:12 +0900

    feat(agnocastlib): introduce bridge_manager (#790)
    * feat: intro bridge_manager
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * feat: run
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refatcor: add error
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * docs: add comment
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * fix: build error
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    ---------
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    Co-authored-by: Koichi Imai <45482193+Koichi98@users.noreply.github.com>

---
+++ src/agnocastlib/src/agnocast_bridge_manager.cpp
@@ -0,0 +1,51 @@
+#include "agnocast/agnocast_bridge_manager.hpp"
+
+#include <sys/prctl.h>
+#include <unistd.h>
+
+#include <csignal>
+#include <stdexcept>
+#include <string>
+
+namespace agnocast
+{
+
+BridgeManager::BridgeManager(pid_t target_pid)
+: target_pid_(target_pid),
+  logger_(rclcpp::get_logger("agnocast_bridge_manager")),
+  event_loop_(target_pid, logger_),
+  loader_(logger_)
+{
+  // Optimization: Fail-fast to avoid rclcpp::init overhead.
+  // Note that the process ensures correct termination even without this check.
+  if (kill(target_pid_, 0) != 0) {
+    throw std::runtime_error("Target parent process is already dead.");
+  }
+
+  if (rclcpp::ok()) {
+    rclcpp::shutdown();
+  }
+
+  rclcpp::InitOptions init_options{};
+  init_options.shutdown_on_signal = false;
+  rclcpp::init(0, nullptr, init_options);
+
+  // TODO(yutarokobayashi): heaphook init
+}
+
+BridgeManager::~BridgeManager()
+{
+  if (rclcpp::ok()) {
+    rclcpp::shutdown();
+  }
+}
+
+void BridgeManager::run()  // NOLINT(readability-convert-member-functions-to-static)
+{
+  std::string proc_name = "agno_br_" + std::to_string(getpid());
+  prctl(PR_SET_NAME, proc_name.c_str(), 0, 0, 0);
+
+  // TODO(yutarokobayashi): event_loop_;
+}
+
+}  // namespace agnocast
