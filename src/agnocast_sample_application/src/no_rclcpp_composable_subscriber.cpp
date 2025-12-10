6ce747d82022037e9882b937510cbccf4787af4c
Koichi <koichi.imai.2@tier4.jp>
2025-12-10 20:23:28 +0900

    add sample app for agnocast::Node
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>

--- src/agnocast_sample_application/src/no_rclcpp_composable_subscriber.cpp
+++ src/agnocast_sample_application/src/no_rclcpp_composable_subscriber.cpp
@@ -1,17 +1,14 @@
 #include "agnocast/agnocast.hpp"
 #include "agnocast_sample_interfaces/msg/dynamic_size_array.hpp"
+#include "rclcpp_components/register_node_macro.hpp"
 
 #include <iostream>
 
 using std::placeholders::_1;
 
-class NoRclcppSubscriber : public agnocast::Node
+class NoRclcppComposableSubscriber : public agnocast::Node
 {
   agnocast::Subscription<agnocast_sample_interfaces::msg::DynamicSizeArray>::SharedPtr sub_dynamic_;
-
-  // Parameters
-  std::string topic_name_;
-  int64_t queue_size_;
 
   void callback(
     const agnocast::ipc_shared_ptr<agnocast_sample_interfaces::msg::DynamicSizeArray> & message)
@@ -21,45 +18,40 @@ class NoRclcppSubscriber : public agnocast::Node
   }
 
 public:
-  explicit NoRclcppSubscriber() : agnocast::Node("no_rclcpp_subscriber")
+  explicit NoRclcppComposableSubscriber(const rclcpp::NodeOptions & options)
+  : agnocast::Node("no_rclcpp_composable_subscriber", options)
   {
     // Declare parameters with default values
     declare_parameter("topic_name", rclcpp::ParameterValue(std::string("my_topic")));
     declare_parameter("queue_size", rclcpp::ParameterValue(int64_t(1)));
 
     // Get parameter values
-    get_parameter("topic_name", topic_name_);
-    get_parameter("queue_size", queue_size_);
+    std::string topic_name;
+    int64_t queue_size;
+    get_parameter("topic_name", topic_name);
+    get_parameter("queue_size", queue_size);
 
     // Resolve topic name (handles remapping and namespace)
-    std::string resolved_topic = resolve_topic_name(topic_name_);
+    std::string resolved_topic = resolve_topic_name(topic_name);
 
     // Log node info and parameters
-    RCLCPP_INFO(get_logger(), "=== NoRclcppSubscriber Node Info ===");
+    RCLCPP_INFO(get_logger(), "=== NoRclcppComposableSubscriber Node Info ===");
     RCLCPP_INFO(get_logger(), "Node name: %s", get_name().c_str());
     RCLCPP_INFO(get_logger(), "Namespace: %s", get_namespace().c_str());
     RCLCPP_INFO(get_logger(), "Fully qualified name: %s", get_fully_qualified_name().c_str());
-    RCLCPP_INFO(get_logger(), "Topic name (input): %s", topic_name_.c_str());
+    RCLCPP_INFO(get_logger(), "Topic name (input): %s", topic_name.c_str());
     RCLCPP_INFO(get_logger(), "Topic name (resolved): %s", resolved_topic.c_str());
-    RCLCPP_INFO(get_logger(), "Queue size: %ld", queue_size_);
-    RCLCPP_INFO(get_logger(), "====================================");
+    RCLCPP_INFO(get_logger(), "Queue size: %ld", queue_size);
+    RCLCPP_INFO(get_logger(), "==============================================");
 
     auto group = create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
     agnocast::SubscriptionOptions agnocast_options;
     agnocast_options.callback_group = group;
 
     sub_dynamic_ = this->create_subscription<agnocast_sample_interfaces::msg::DynamicSizeArray>(
-      resolved_topic, static_cast<size_t>(queue_size_),
-      std::bind(&NoRclcppSubscriber::callback, this, _1), agnocast_options);
+      resolved_topic, static_cast<size_t>(queue_size),
+      std::bind(&NoRclcppComposableSubscriber::callback, this, _1), agnocast_options);
   }
 };
 
-int main(int argc, char ** argv)
-{
-  agnocast::init(argc, argv);
-  agnocast::AgnocastOnlySingleThreadedExecutor executor;
-  auto node = std::make_shared<NoRclcppSubscriber>();
-  executor.add_node(node);
-  executor.spin();
-  return 0;
-}
+RCLCPP_COMPONENTS_REGISTER_NODE(NoRclcppComposableSubscriber)
