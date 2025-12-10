afdbd4e1134b2e5f9d4d672f39c3c5daf1991d18
Yutaro Kobayashi <129580202+kobayu858@users.noreply.github.com>
2025-12-10 19:42:51 +0900

    feat(agnocastlib): implement safe epoll event loop with timeout (#805)
    * refactor: clang-tidy
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: clang-tidy
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * feat: spin_once
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: copolot
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: return value
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    ---------
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    Co-authored-by: Koichi Imai <45482193+Koichi98@users.noreply.github.com>

--- src/agnocastlib/include/agnocast/agnocast_bridge_ipc_event_loop.hpp
+++ src/agnocastlib/include/agnocast/agnocast_bridge_ipc_event_loop.hpp
@@ -11,13 +11,19 @@ class BridgeIpcEventLoop
 {
 public:
   BridgeIpcEventLoop(pid_t target_pid, const rclcpp::Logger & logger);
-  ~BridgeIpcEventLoop() = default;
+  ~BridgeIpcEventLoop();
 
   BridgeIpcEventLoop(const BridgeIpcEventLoop &) = delete;
   BridgeIpcEventLoop & operator=(const BridgeIpcEventLoop &) = delete;
 
+  bool spin_once(int timeout_ms);
+
 private:
   rclcpp::Logger logger_;
+
+  int epoll_fd_ = -1;
+
+  void setup_epoll();
 };
 
 }  // namespace agnocast
