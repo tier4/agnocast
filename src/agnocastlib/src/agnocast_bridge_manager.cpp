5f9f61027a2e520aa27b3e53e1b08db059bd2983
kobayu858 <yutaro.kobayashi.2@tier4.jp>
2025-12-11 20:08:58 +0900

    feat: use
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

--- src/agnocastlib/src/agnocast_bridge_manager.cpp
+++ src/agnocastlib/src/agnocast_bridge_manager.cpp
@@ -120,16 +120,21 @@ void BridgeManager::handle_create_request(const MqMsgBridge & req, bool /*allow_
   }
 
   // TODO(yutarokobayashi): The following comments are scheduled for implementation in a later PR.
-  // Attempt to register the bridge with the kernel
+  struct ioctl_add_bridge_args add_bridge_args;
+  std::memset(&add_bridge_args, 0, sizeof(add_bridge_args));
+  add_bridge_args.pid = getpid();
+  add_bridge_args.topic_name = {topic_name_.c_str(), topic_name_.size()};
 
-  // Registration successful: Load and create the bridge instance
-  // Rollback kernel registration if bridge creation fails
-
-  // The bridge is already registered in the kernel (EEXIST case)
-  // If allow_delegation is true, retrieve the PID of the current owner and delegate.
-  // Otherwise, abort to avoid loops.
-
-  // Handle unexpected ioctl errors
+  if (ioctl(agnocast_fd, AGNOCAST_ADD_BRIDGE_CMD, &add_bridge_args) == 0) {
+    // Registration successful: Load and create the bridge instance
+    // Rollback kernel registration if bridge creation fails
+  } else if (errno == EEXIST) {
+    // The bridge is already registered in the kernel (EEXIST case)
+    // If allow_delegation is true, retrieve the PID of the current owner and delegate.
+    // Otherwise, abort to avoid loops.
+  } else {
+    // Handle unexpected ioctl errors
+  }
 }
 
 void BridgeManager::check_parent_alive()
