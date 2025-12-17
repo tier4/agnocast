7f599879376ca02b0b8d22914a432b130a903e01
Koichi Imai <koichi.imai.2@tier4.jp>
2025-12-17 16:28:58 +0900

    add declare/has/get_parameter functions for Node
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- src/agnocast_sample_application/src/no_rclcpp_subscriber.cpp
+++ src/agnocast_sample_application/src/no_rclcpp_subscriber.cpp
@@ -9,6 +9,10 @@ class NoRclcppSubscriber : public agnocast::Node
 {
   agnocast::Subscription<agnocast_sample_interfaces::msg::DynamicSizeArray>::SharedPtr sub_dynamic_;
 
+  // Parameters
+  std::string topic_name_;
+  int64_t queue_size_;
+
   void callback(
     const agnocast::ipc_shared_ptr<agnocast_sample_interfaces::msg::DynamicSizeArray> & message)
   {
@@ -19,14 +23,30 @@ class NoRclcppSubscriber : public agnocast::Node
 public:
   explicit NoRclcppSubscriber() : agnocast::Node("no_rclcpp_subscriber")
   {
-    RCLCPP_INFO(get_logger(), "NoRclcppSubscriber node (name=%s) started.", get_name().c_str());
+    // Declare parameters with default values
+    declare_parameter("topic_name", rclcpp::ParameterValue(std::string("/my_topic")));
+    declare_parameter("queue_size", rclcpp::ParameterValue(int64_t(1)));
 
-    auto group = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+    // Get parameter values
+    get_parameter("topic_name", topic_name_);
+    get_parameter("queue_size", queue_size_);
+
+    // Log node info and parameters
+    RCLCPP_INFO(get_logger(), "=== NoRclcppSubscriber Node Info ===");
+    RCLCPP_INFO(get_logger(), "Node name: %s", get_name().c_str());
+    RCLCPP_INFO(get_logger(), "Namespace: %s", get_namespace().c_str());
+    RCLCPP_INFO(get_logger(), "Fully qualified name: %s", get_fully_qualified_name().c_str());
+    RCLCPP_INFO(get_logger(), "Topic name: %s", topic_name_.c_str());
+    RCLCPP_INFO(get_logger(), "Queue size: %ld", queue_size_);
+    RCLCPP_INFO(get_logger(), "====================================");
+
+    auto group = create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
     agnocast::SubscriptionOptions agnocast_options;
     agnocast_options.callback_group = group;
 
     sub_dynamic_ = this->create_subscription<agnocast_sample_interfaces::msg::DynamicSizeArray>(
-      "/my_topic", 1, std::bind(&NoRclcppSubscriber::callback, this, _1), agnocast_options);
+      topic_name_, static_cast<size_t>(queue_size_),
+      std::bind(&NoRclcppSubscriber::callback, this, _1), agnocast_options);
   }
 };
 
