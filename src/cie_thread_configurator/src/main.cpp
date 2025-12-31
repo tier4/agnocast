d159cadb1668b3f8921443aaaa9c5c38ca233370
Koichi <koichi.imai.2@tier4.jp>
2025-12-31 21:12:57 +0900

    delete copyright
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>

--- src/cie_thread_configurator/src/main.cpp
+++ src/cie_thread_configurator/src/main.cpp
@@ -1,17 +1,3 @@
-// Copyright 2024 The Agnocast Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #include "cie_thread_configurator/cie_thread_configurator.hpp"
 #include "cie_thread_configurator/prerun_node.hpp"
 #include "cie_thread_configurator/thread_configurator_node.hpp"
