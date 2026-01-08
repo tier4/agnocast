1794d840c8215904b037b6acc5bb4c6f69090497
Yutaro Kobayashi <129580202+kobayu858@users.noreply.github.com>
2026-01-08 10:32:58 +0900

    feat(agnocastlib): performance eventloop (#901)
    * feat: performance_event_loop
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: minor
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: unification_event_loop
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: remove cpp
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: minor
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: minor
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: handler
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: remove
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    ---------
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

--- src/agnocastlib/src/bridge/agnocast_performance_bridge_ipc_event_loop.cpp
+++ src/agnocastlib/src/bridge/agnocast_performance_bridge_ipc_event_loop.cpp
@@ -1,21 +1,43 @@
-
 #include "agnocast/bridge/agnocast_performance_bridge_ipc_event_loop.hpp"
 
-#include <rclcpp/logging.hpp>
+#include "agnocast/agnocast_mq.hpp"
+#include "agnocast/agnocast_utils.hpp"
+
+#include <utility>
+#include <vector>
 
 namespace agnocast
 {
 
 PerformanceBridgeIpcEventLoop::PerformanceBridgeIpcEventLoop(const rclcpp::Logger & logger)
-: logger_(logger)
+: IpcEventLoopBase(
+    logger,
+    // 1. MQ Name
+    create_mq_name_for_bridge(PERFORMANCE_BRIDGE_VIRTUAL_PID),
+    // 2. Message Size
+    PERFORMANCE_BRIDGE_MQ_MESSAGE_SIZE,
+    // 3. Block Signals
+    {SIGTERM, SIGINT, SIGHUP},
+    // 4. Ignore Signals
+    {SIGPIPE})
 {
   RCLCPP_INFO(logger_, "PerformanceBridgeIpcEventLoop initialized (Skeleton).");
 }
 
-PerformanceBridgeIpcEventLoop::~PerformanceBridgeIpcEventLoop() = default;
-
-void PerformanceBridgeIpcEventLoop::spin_once()
+void PerformanceBridgeIpcEventLoop::set_reload_handler(ReloadCallback cb)
 {
+  reload_cb_ = std::move(cb);
+}
+
+void PerformanceBridgeIpcEventLoop::handle_signal(int signo)
+{
+  IpcEventLoopBase::handle_signal(signo);
+
+  if (signo == SIGHUP) {
+    if (reload_cb_) {
+      reload_cb_();
+    }
+  }
 }
 
 }  // namespace agnocast
