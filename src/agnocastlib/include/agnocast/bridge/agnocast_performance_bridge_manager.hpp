52c43ad4e98aa72c4922e9a76e685e5dc43907f5
Yutaro Kobayashi <129580202+kobayu858@users.noreply.github.com>
2026-01-09 12:28:14 +0900

    feat(agnocastlib): send performance request (#906)
    * feat: send performance request
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: unify
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: rename
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: logger name
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: clang-tidy
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    ---------
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

--- src/agnocastlib/include/agnocast/bridge/agnocast_performance_bridge_manager.hpp
+++ src/agnocastlib/include/agnocast/bridge/agnocast_performance_bridge_manager.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
+#include "agnocast/agnocast_multi_threaded_executor.hpp"
 #include "agnocast/bridge/agnocast_performance_bridge_config.hpp"
 #include "agnocast/bridge/agnocast_performance_bridge_ipc_event_loop.hpp"
 #include "agnocast/bridge/agnocast_performance_bridge_loader.hpp"
 
 #include <rclcpp/rclcpp.hpp>
+
+#include <memory>
+#include <thread>
 
 namespace agnocast
 {
@@ -23,7 +27,17 @@ private:
   PerformanceBridgeLoader loader_;
   PerformanceBridgeConfig config_;
 
+  std::shared_ptr<rclcpp::Node> container_node_;
+  std::shared_ptr<agnocast::MultiThreadedAgnocastExecutor> executor_;
+  std::thread executor_thread_;
+
   bool shutdown_requested_ = false;
+
+  void start_ros_execution();
+
+  void on_mq_request(int fd);
+  void on_signal();
+  void on_reload();
 
   void check_and_request_shutdown();
 };
