ac4e8a5744f6b984d9fe574480cffafdac20cf63
Koichi Imai <koichi.imai.2@tier4.jp>
2026-01-15 20:09:09 +0900

    add get_topic_name interface
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- src/agnocastlib/include/agnocast/agnocast_publisher.hpp
+++ src/agnocastlib/include/agnocast/agnocast_publisher.hpp
@@ -165,6 +165,8 @@ public:
   // Note: ROS 2 subscriber count is updated by the Bridge Manager periodically.
   // TODO(Koichi98): It just returns the number of Agnocast subscribers for performance bridge.
   uint32_t get_subscription_count() const { return get_subscription_count_core(topic_name_); }
+
+  const char * get_topic_name() const { return topic_name_.c_str(); }
 };
 
 struct AgnocastToRosRequestPolicy;
