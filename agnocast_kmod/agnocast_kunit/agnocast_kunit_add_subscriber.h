a8c325ea46a4d6398cc69f3334ab84f49142f0cf
atsushi421 <atsushi.yano.2@tier4.jp>
2026-01-19 19:54:24 +0900

    clang-format

--- agnocast_kmod/agnocast_kunit/agnocast_kunit_add_subscriber.h
+++ agnocast_kmod/agnocast_kunit/agnocast_kunit_add_subscriber.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <kunit/test.h>
 
-#define TEST_CASES_ADD_SUBSCRIBER \
-  KUNIT_CASE(test_case_add_subscriber_normal), KUNIT_CASE(test_case_add_subscriber_too_many_subscribers)
+#define TEST_CASES_ADD_SUBSCRIBER              \
+  KUNIT_CASE(test_case_add_subscriber_normal), \
+    KUNIT_CASE(test_case_add_subscriber_too_many_subscribers)
 
 void test_case_add_subscriber_normal(struct kunit * test);
 void test_case_add_subscriber_too_many_subscribers(struct kunit * test);
