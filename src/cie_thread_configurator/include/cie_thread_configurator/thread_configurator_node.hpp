52b3ed040ef17272bd8489c0ee650af1e66b669d
atsushi yano <55824710+atsushi421@users.noreply.github.com>
2026-01-23 03:19:10 +0900

    feat(cie_thread_configurator): support multiple ROS domain (#951)
    * feat: support multiple ROS domain
    
    * fix

--- src/cie_thread_configurator/include/cie_thread_configurator/thread_configurator_node.hpp
+++ src/cie_thread_configurator/include/cie_thread_configurator/thread_configurator_node.hpp
@@ -6,7 +6,6 @@
 #include "cie_config_msgs/msg/callback_group_info.hpp"
 
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 class ThreadConfiguratorNode : public rclcpp::Node
@@ -14,6 +13,7 @@ class ThreadConfiguratorNode : public rclcpp::Node
   struct CallbackGroupConfig
   {
     std::string callback_group_id;
+    size_t domain_id;
     int64_t thread_id = -1;
     std::vector<int> affinity;
     std::string policy;
@@ -36,15 +36,21 @@ public:
   bool exist_deadline_config();
   bool apply_deadline_configs();
 
+  const std::vector<rclcpp::Node::SharedPtr> & get_domain_nodes() const;
+
 private:
   bool set_affinity_by_cgroup(int64_t thread_id, const std::vector<int> & cpus);
   bool issue_syscalls(const CallbackGroupConfig & config);
-  void topic_callback(const cie_config_msgs::msg::CallbackGroupInfo::SharedPtr msg);
+  void topic_callback(
+    size_t domain_id, const cie_config_msgs::msg::CallbackGroupInfo::SharedPtr msg);
 
-  rclcpp::Subscription<cie_config_msgs::msg::CallbackGroupInfo>::SharedPtr subscription_;
+  std::vector<rclcpp::Node::SharedPtr> nodes_for_each_domain_;
+  std::vector<rclcpp::Subscription<cie_config_msgs::msg::CallbackGroupInfo>::SharedPtr>
+    subs_for_each_domain_;
 
   std::vector<CallbackGroupConfig> callback_group_configs_;
-  std::unordered_map<std::string, CallbackGroupConfig *> id_to_callback_group_config_;
+  // (domain_id, callback_group_id) -> CallbackGroupConfig*
+  std::map<std::pair<size_t, std::string>, CallbackGroupConfig *> id_to_callback_group_config_;
   int unapplied_num_;
   int cgroup_num_;
 
