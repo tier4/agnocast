cf644c0d10522ab0a9825d521d911454014643ca
Takahiro Ishikawa-Aso <sykwer@gmail.com>
2026-01-26 09:52:17 +0900

    feat: support jazzy (#961)
    * jazzy support in agnocastlib
    
    Signed-off-by: sykwer <sykwer@gmail.com>
    
    * support jazzy in ci
    
    Signed-off-by: sykwer <sykwer@gmail.com>
    
    * fix
    
    Signed-off-by: sykwer <sykwer@gmail.com>
    
    * fix
    
    Signed-off-by: sykwer <sykwer@gmail.com>
    
    * fix
    
    Signed-off-by: sykwer <sykwer@gmail.com>
    
    * fix
    
    Signed-off-by: sykwer <sykwer@gmail.com>
    
    * fix
    
    Signed-off-by: sykwer <sykwer@gmail.com>
    
    * fix
    
    Signed-off-by: sykwer <sykwer@gmail.com>
    
    * fix
    
    Signed-off-by: sykwer <sykwer@gmail.com>
    
    * fix
    
    Signed-off-by: sykwer <sykwer@gmail.com>
    
    * fix
    
    Signed-off-by: sykwer <sykwer@gmail.com>
    
    * fix
    
    Signed-off-by: sykwer <sykwer@gmail.com>
    
    * fix
    
    Signed-off-by: sykwer <sykwer@gmail.com>
    
    * add readme
    
    Signed-off-by: sykwer <sykwer@gmail.com>
    
    * add comment
    
    Signed-off-by: sykwer <sykwer@gmail.com>
    
    ---------
    
    Signed-off-by: sykwer <sykwer@gmail.com>

--- src/agnocastlib/src/node/node_interfaces/node_logging.cpp
+++ src/agnocastlib/src/node/node_interfaces/node_logging.cpp
@@ -1,5 +1,7 @@
 #include "agnocast/node/node_interfaces/node_logging.hpp"
 
+#include <stdexcept>
+
 namespace agnocast::node_interfaces
 {
 
@@ -16,5 +18,15 @@ const char * NodeLogging::get_logger_name() const
 {
   return logger_.get_name();
 }
+
+// rclcpp 28+ (Jazzy) added this method to NodeLoggingInterface.
+// This is a stub implementation - logger services are not yet supported.
+#if RCLCPP_VERSION_MAJOR >= 28
+void NodeLogging::create_logger_services(
+  rclcpp::node_interfaces::NodeServicesInterface::SharedPtr /* node_services */)
+{
+  throw std::runtime_error("create_logger_services is not yet implemented");
+}
+#endif
 
 }  // namespace agnocast::node_interfaces
