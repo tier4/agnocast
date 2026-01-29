fad7a5b56c58aa567b128817e9945f7933953d21
Koichi Imai <koichi.imai.2@tier4.jp>
2026-01-29 16:51:06 +0900

    fix
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- src/agnocastlib/include/agnocast/node/tf2/buffer.hpp
+++ src/agnocastlib/include/agnocast/node/tf2/buffer.hpp
@@ -42,31 +42,34 @@ public:
   using tf2::BufferCore::lookupTransform;
   using SharedPtr = std::shared_ptr<Buffer>;
 
-  /// \brief Constructor
-  /// \param clock The clock to use for timing operations
-  /// \param cache_time How long to keep transform history (default: 10 seconds)
+  /** \brief  Constructor for a Buffer object
+   * \param clock A clock to use for time and sleeping
+   * \param cache_time How long to keep a history of transforms
+   */
   explicit Buffer(
     rclcpp::Clock::SharedPtr clock,
     tf2::Duration cache_time = tf2::Duration(tf2::BUFFER_CORE_DEFAULT_CACHE_TIME));
 
   virtual ~Buffer() = default;
 
-  // ========== Synchronous lookupTransform ==========
-
-  /// \brief Get the transform between two frames by frame ID.
-  ///
-  /// \param target_frame The frame to which data should be transformed
-  /// \param source_frame The frame where the data originated
-  /// \param time The time at which the value of the transform is desired (0 = latest)
-  /// \param timeout How long to block before failing
-  /// \return The transform between the frames
-  /// \throws tf2::LookupException, tf2::ConnectivityException,
-  ///         tf2::ExtrapolationException, tf2::InvalidArgumentException
+  /** \brief Get the transform between two frames by frame ID.
+   * \param target_frame The frame to which data should be transformed
+   * \param source_frame The frame where the data originated
+   * \param time The time at which the value of the transform is desired. (0 will get the latest)
+   * \param timeout How long to block before failing
+   * \return The transform between the frames
+   *
+   * Possible exceptions tf2::LookupException, tf2::ConnectivityException,
+   * tf2::ExtrapolationException, tf2::InvalidArgumentException
+   */
   geometry_msgs::msg::TransformStamped lookupTransform(
     const std::string & target_frame, const std::string & source_frame, const tf2::TimePoint & time,
     const tf2::Duration timeout) const override;
 
-  /// \brief Get the transform between two frames by frame ID (rclcpp overload).
+  /** \brief Get the transform between two frames by frame ID.
+   * \sa lookupTransform(const std::string&, const std::string&, const tf2::TimePoint&,
+   *                     const tf2::Duration)
+   */
   geometry_msgs::msg::TransformStamped lookupTransform(
     const std::string & target_frame, const std::string & source_frame, const rclcpp::Time & time,
     const rclcpp::Duration timeout = rclcpp::Duration::from_nanoseconds(0)) const
@@ -74,13 +77,29 @@ public:
     return lookupTransform(target_frame, source_frame, fromRclcpp(time), fromRclcpp(timeout));
   }
 
-  /// \brief Get the transform between two frames by frame ID (advanced).
+  /** \brief Get the transform between two frames by frame ID assuming fixed frame.
+   * \param target_frame The frame to which data should be transformed
+   * \param target_time The time to which the data should be transformed. (0 will get the latest)
+   * \param source_frame The frame where the data originated
+   * \param source_time The time at which the source_frame should be evaluated. (0 will get the
+   * latest)
+   * \param fixed_frame The frame in which to assume the transform is constant in time.
+   * \param timeout How long to block before failing
+   * \return The transform between the frames
+   *
+   * Possible exceptions tf2::LookupException, tf2::ConnectivityException,
+   * tf2::ExtrapolationException, tf2::InvalidArgumentException
+   */
   geometry_msgs::msg::TransformStamped lookupTransform(
     const std::string & target_frame, const tf2::TimePoint & target_time,
     const std::string & source_frame, const tf2::TimePoint & source_time,
     const std::string & fixed_frame, const tf2::Duration timeout) const override;
 
-  /// \brief Get the transform between two frames by frame ID (advanced, rclcpp overload).
+  /** \brief Get the transform between two frames by frame ID assuming fixed frame.
+   * \sa lookupTransform(const std::string&, const tf2::TimePoint&,
+   *                     const std::string&, const tf2::TimePoint&,
+   *                     const std::string&, const tf2::Duration)
+   */
   geometry_msgs::msg::TransformStamped lookupTransform(
     const std::string & target_frame, const rclcpp::Time & target_time,
     const std::string & source_frame, const rclcpp::Time & source_time,
@@ -92,77 +111,91 @@ public:
       fromRclcpp(timeout));
   }
 
-  // ========== Synchronous canTransform ==========
-
-  /// \brief Test if a transform is possible
+  /** \brief Test if a transform is possible
+   * \param target_frame The frame into which to transform
+   * \param source_frame The frame from which to transform
+   * \param target_time The time at which to transform
+   * \param timeout How long to block before failing
+   * \param errstr A pointer to a string which will be filled with why the transform failed, if not
+   * NULL
+   * \return True if the transform is possible, false otherwise
+   */
   bool canTransform(
     const std::string & target_frame, const std::string & source_frame, const tf2::TimePoint & time,
-    const tf2::Duration timeout, std::string * errstr = nullptr) const override;
+    const tf2::Duration timeout, std::string * errstr = NULL) const override;
 
-  /// \brief Test if a transform is possible (rclcpp overload)
+  /** \brief Test if a transform is possible
+   * \sa canTransform(const std::string&, const std::string&,
+   *                  const tf2::TimePoint&, const tf2::Duration, std::string*)
+   */
   bool canTransform(
     const std::string & target_frame, const std::string & source_frame, const rclcpp::Time & time,
     const rclcpp::Duration timeout = rclcpp::Duration::from_nanoseconds(0),
-    std::string * errstr = nullptr) const
+    std::string * errstr = NULL) const
   {
     return canTransform(target_frame, source_frame, fromRclcpp(time), fromRclcpp(timeout), errstr);
   }
 
-  /// \brief Test if a transform is possible (advanced)
+  /** \brief Test if a transform is possible
+   * \param target_frame The frame into which to transform
+   * \param target_time The time into which to transform
+   * \param source_frame The frame from which to transform
+   * \param source_time The time from which to transform
+   * \param fixed_frame The frame in which to treat the transform as constant in time
+   * \param timeout How long to block before failing
+   * \param errstr A pointer to a string which will be filled with why the transform failed, if not
+   * NULL
+   * \return True if the transform is possible, false otherwise
+   */
   bool canTransform(
     const std::string & target_frame, const tf2::TimePoint & target_time,
     const std::string & source_frame, const tf2::TimePoint & source_time,
     const std::string & fixed_frame, const tf2::Duration timeout,
-    std::string * errstr = nullptr) const override;
+    std::string * errstr = NULL) const override;
 
-  /// \brief Test if a transform is possible (advanced, rclcpp overload)
+  /** \brief Test if a transform is possible
+   * \sa
+   *   canTransform(const std::string&, const tf2::TimePoint&,
+   *                const std::string&, const tf2::TimePoint&,
+   *                const std::string&, const tf2::Duration, std::string*)
+   */
   bool canTransform(
     const std::string & target_frame, const rclcpp::Time & target_time,
     const std::string & source_frame, const rclcpp::Time & source_time,
     const std::string & fixed_frame,
     const rclcpp::Duration timeout = rclcpp::Duration::from_nanoseconds(0),
-    std::string * errstr = nullptr) const
+    std::string * errstr = NULL) const
   {
     return canTransform(
       target_frame, fromRclcpp(target_time), source_frame, fromRclcpp(source_time), fixed_frame,
       fromRclcpp(timeout), errstr);
   }
 
-  // ========== Configuration ==========
-
   /// \brief Get the clock used by this buffer
   rclcpp::Clock::SharedPtr getClock() const { return clock_; }
 
   /// \brief Set whether a dedicated thread is being used for TF updates.
-  ///
-  /// This is required before using canTransform or lookupTransform with a non-zero timeout.
-  ///
-  /// \param value True if a dedicated thread is servicing TF messages
   void setUsingDedicatedThread(bool value) { using_dedicated_thread_ = value; }
 
   /// \brief Check if a dedicated thread is being used for TF updates.
   bool isUsingDedicatedThread() const { return using_dedicated_thread_; }
 
 private:
+  /// \brief A clock to use for time and sleeping
   rclcpp::Clock::SharedPtr clock_;
 
-  /// \brief Whether a dedicated thread is servicing TF messages
   bool using_dedicated_thread_ = false;
 
   /// \brief Reference to a jump handler registered to the clock
   rclcpp::JumpHandler::SharedPtr jump_handler_;
 
-  /// \brief Callback for time jumps (clears buffer on backward jumps or clock changes)
   void onTimeJump(const rcl_time_jump_t & jump);
 
-  /// \brief Check if dedicated thread is present, log error if not
+  // conditionally error if dedicated_thread unset.
   bool checkAndErrorDedicatedThreadPresent(std::string * errstr) const;
 
-  /// \brief Wait for transform to become available (polling-based, for sync operations)
-  /// \return true if transform became available, false on timeout
-  bool waitForTransformAvailable(
-    const std::string & target_frame, const std::string & source_frame, const tf2::TimePoint & time,
-    const tf2::Duration & timeout, std::string * errstr = nullptr) const;
+  /// Get the logger to use for calls to RCLCPP log macros.
+  rclcpp::Logger getLogger() const;
 };
 
 }  // namespace agnocast
--- src/agnocastlib/src/node/tf2/buffer.cpp
+++ src/agnocastlib/src/node/tf2/buffer.cpp
@@ -1,31 +1,25 @@
 #include "agnocast/node/tf2/buffer.hpp"
 
-#include "tf2/exceptions.h"
-#include "tf2_ros/buffer_interface.h"
-
-#include <chrono>
+#include <sstream>
+#include <string>
 #include <thread>
 
 namespace agnocast
 {
 
-namespace
-{
-constexpr char threading_error[] =
+static const char threading_error[] =
   "Do not call canTransform or lookupTransform with a timeout "
   "unless you are using another thread for populating data. Without a dedicated thread it will "
-  "always timeout. If you have a separate thread servicing tf messages, call "
+  "always timeout.  If you have a separate thread servicing tf messages, call "
   "setUsingDedicatedThread(true) on your Buffer instance.";
-}  // namespace
 
 Buffer::Buffer(rclcpp::Clock::SharedPtr clock, tf2::Duration cache_time)
-: tf2::BufferCore(cache_time), clock_(clock)
+: BufferCore(cache_time), clock_(clock)
 {
   if (nullptr == clock_) {
     throw std::invalid_argument("clock must be a valid instance");
   }
 
-  // Set up time jump callback
   auto post_jump_cb = [this](const rcl_time_jump_t & jump_info) { onTimeJump(jump_info); };
 
   rcl_jump_threshold_t jump_threshold;
@@ -39,19 +33,118 @@ Buffer::Buffer(rclcpp::Clock::SharedPtr clock, tf2::Duration cache_time)
   jump_handler_ = clock_->create_jump_callback(nullptr, post_jump_cb, jump_threshold);
 }
 
+inline tf2::Duration from_rclcpp(const rclcpp::Duration & rclcpp_duration)
+{
+  return tf2::Duration(std::chrono::nanoseconds(rclcpp_duration.nanoseconds()));
+}
+
+inline rclcpp::Duration to_rclcpp(const tf2::Duration & duration)
+{
+  return rclcpp::Duration(std::chrono::nanoseconds(duration));
+}
+
+geometry_msgs::msg::TransformStamped Buffer::lookupTransform(
+  const std::string & target_frame, const std::string & source_frame,
+  const tf2::TimePoint & lookup_time, const tf2::Duration timeout) const
+{
+  // Pass error string to suppress console spam
+  std::string error;
+  canTransform(target_frame, source_frame, lookup_time, timeout, &error);
+  return lookupTransform(target_frame, source_frame, lookup_time);
+}
+
 void Buffer::onTimeJump(const rcl_time_jump_t & jump)
 {
   if (
     RCL_ROS_TIME_ACTIVATED == jump.clock_change || RCL_ROS_TIME_DEACTIVATED == jump.clock_change) {
-    RCLCPP_WARN(
-      rclcpp::get_logger("agnocast_tf2_buffer"),
-      "Detected time source change. Clearing TF buffer.");
+    RCLCPP_WARN(getLogger(), "Detected time source change. Clearing TF buffer.");
     clear();
   } else if (jump.delta.nanoseconds < 0) {
-    RCLCPP_WARN(
-      rclcpp::get_logger("agnocast_tf2_buffer"), "Detected jump back in time. Clearing TF buffer.");
+    RCLCPP_WARN(getLogger(), "Detected jump back in time. Clearing TF buffer.");
     clear();
   }
+}
+
+geometry_msgs::msg::TransformStamped Buffer::lookupTransform(
+  const std::string & target_frame, const tf2::TimePoint & target_time,
+  const std::string & source_frame, const tf2::TimePoint & source_time,
+  const std::string & fixed_frame, const tf2::Duration timeout) const
+{
+  // Pass error string to suppress console spam
+  std::string error;
+  canTransform(target_frame, target_time, source_frame, source_time, fixed_frame, timeout, &error);
+  return lookupTransform(target_frame, target_time, source_frame, source_time, fixed_frame);
+}
+
+void conditionally_append_timeout_info(
+  std::string * errstr, const rclcpp::Time & start_time, const rclcpp::Time & current_time,
+  const rclcpp::Duration & timeout)
+{
+  if (errstr) {
+    std::stringstream ss;
+    ss << ". canTransform returned after "
+       << tf2::durationToSec(from_rclcpp(current_time - start_time)) << " timeout was "
+       << tf2::durationToSec(from_rclcpp(timeout)) << ".";
+    (*errstr) += ss.str();
+  }
+}
+
+bool Buffer::canTransform(
+  const std::string & target_frame, const std::string & source_frame, const tf2::TimePoint & time,
+  const tf2::Duration timeout, std::string * errstr) const
+{
+  if (!checkAndErrorDedicatedThreadPresent(errstr)) {
+    return false;
+  }
+
+  rclcpp::Duration rclcpp_timeout(to_rclcpp(timeout));
+
+  // poll for transform if timeout is set
+  rclcpp::Time start_time = clock_->now();
+  while (
+    clock_->now() < start_time + rclcpp_timeout &&
+    !canTransform(
+      target_frame, source_frame, time, tf2::Duration(std::chrono::nanoseconds::zero()), errstr) &&
+    (clock_->now() + rclcpp::Duration(3, 0) >= start_time) &&  // don't wait bag loop detected
+    (rclcpp::ok()))  // Make sure we haven't been stopped (won't work for pytf)
+  {
+    // TODO(sloretz) sleep using clock_->sleep_for when implemented
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
+  bool retval = canTransform(target_frame, source_frame, time, errstr);
+  rclcpp::Time current_time = clock_->now();
+  conditionally_append_timeout_info(errstr, start_time, current_time, rclcpp_timeout);
+  return retval;
+}
+
+bool Buffer::canTransform(
+  const std::string & target_frame, const tf2::TimePoint & target_time,
+  const std::string & source_frame, const tf2::TimePoint & source_time,
+  const std::string & fixed_frame, const tf2::Duration timeout, std::string * errstr) const
+{
+  if (!checkAndErrorDedicatedThreadPresent(errstr)) {
+    return false;
+  }
+
+  rclcpp::Duration rclcpp_timeout(to_rclcpp(timeout));
+
+  // poll for transform if timeout is set
+  rclcpp::Time start_time = clock_->now();
+  while (clock_->now() < start_time + rclcpp_timeout &&
+         !canTransform(
+           target_frame, target_time, source_frame, source_time, fixed_frame,
+           tf2::Duration(std::chrono::nanoseconds::zero()), errstr) &&
+         (clock_->now() + rclcpp::Duration(3, 0) >= start_time) &&  // don't wait bag loop detected
+         (rclcpp::ok()))  // Make sure we haven't been stopped (won't work for pytf)
+  {
+    // TODO(sloretz) sleep using clock_->sleep_for when implemented
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
+  bool retval =
+    canTransform(target_frame, target_time, source_frame, source_time, fixed_frame, errstr);
+  rclcpp::Time current_time = clock_->now();
+  conditionally_append_timeout_info(errstr, start_time, current_time, rclcpp_timeout);
+  return retval;
 }
 
 bool Buffer::checkAndErrorDedicatedThreadPresent(std::string * error_str) const
@@ -64,87 +157,13 @@ bool Buffer::checkAndErrorDedicatedThreadPresent(std::string * error_str) const
     *error_str = threading_error;
   }
 
-  RCLCPP_ERROR(rclcpp::get_logger("agnocast_tf2_buffer"), "%s", threading_error);
+  RCLCPP_ERROR(getLogger(), "%s", threading_error);
   return false;
 }
 
-bool Buffer::waitForTransformAvailable(
-  const std::string & target_frame, const std::string & source_frame, const tf2::TimePoint & time,
-  const tf2::Duration & timeout, std::string * errstr) const
+rclcpp::Logger Buffer::getLogger() const
 {
-  // If timeout is zero, don't wait
-  if (timeout <= tf2::Duration::zero()) {
-    return BufferCore::canTransform(target_frame, source_frame, time, errstr);
-  }
-
-  const auto start_time = clock_->now();
-  const auto timeout_duration = std::chrono::duration_cast<std::chrono::nanoseconds>(timeout);
-  const auto end_time = start_time + rclcpp::Duration(timeout_duration);
-
-  // Polling interval (10ms, same as tf2_ros::Buffer)
-  const auto poll_interval = std::chrono::milliseconds(10);
-
-  while (clock_->now() < end_time &&
-         !BufferCore::canTransform(target_frame, source_frame, time, errstr) &&
-         (clock_->now() + rclcpp::Duration(3, 0) >= start_time) &&  // don't wait if bag loop
-         rclcpp::ok()) {
-    std::this_thread::sleep_for(poll_interval);
-  }
-
-  // Final check after timeout
-  return BufferCore::canTransform(target_frame, source_frame, time, errstr);
-}
-
-geometry_msgs::msg::TransformStamped Buffer::lookupTransform(
-  const std::string & target_frame, const std::string & source_frame, const tf2::TimePoint & time,
-  const tf2::Duration timeout) const
-{
-  // Pass error string to suppress console spam
-  std::string error;
-  canTransform(target_frame, source_frame, time, timeout, &error);
-  return BufferCore::lookupTransform(target_frame, source_frame, time);
-}
-
-geometry_msgs::msg::TransformStamped Buffer::lookupTransform(
-  const std::string & target_frame, const tf2::TimePoint & target_time,
-  const std::string & source_frame, const tf2::TimePoint & source_time,
-  const std::string & fixed_frame, const tf2::Duration timeout) const
-{
-  // Pass error string to suppress console spam
-  std::string error;
-  canTransform(target_frame, target_time, source_frame, source_time, fixed_frame, timeout, &error);
-  return BufferCore::lookupTransform(
-    target_frame, target_time, source_frame, source_time, fixed_frame);
-}
-
-bool Buffer::canTransform(
-  const std::string & target_frame, const std::string & source_frame, const tf2::TimePoint & time,
-  const tf2::Duration timeout, std::string * errstr) const
-{
-  // Check dedicated thread if timeout is non-zero
-  if (timeout > tf2::Duration::zero() && !checkAndErrorDedicatedThreadPresent(errstr)) {
-    return false;
-  }
-  return waitForTransformAvailable(target_frame, source_frame, time, timeout, errstr);
-}
-
-bool Buffer::canTransform(
-  const std::string & target_frame, const tf2::TimePoint & target_time,
-  const std::string & source_frame, const tf2::TimePoint & source_time,
-  const std::string & fixed_frame, const tf2::Duration timeout, std::string * errstr) const
-{
-  // Check dedicated thread if timeout is non-zero
-  if (timeout > tf2::Duration::zero() && !checkAndErrorDedicatedThreadPresent(errstr)) {
-    return false;
-  }
-  // Check if both transforms are available
-  if (!waitForTransformAvailable(target_frame, fixed_frame, target_time, timeout, errstr)) {
-    return false;
-  }
-  if (!waitForTransformAvailable(fixed_frame, source_frame, source_time, timeout, errstr)) {
-    return false;
-  }
-  return true;
+  return rclcpp::get_logger("tf2_buffer");
 }
 
 }  // namespace agnocast
