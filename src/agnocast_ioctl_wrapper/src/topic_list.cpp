9b32195f44d970a3f6354328d77feb7509e2ba30
TetsuKawa <kawaguchitnon@icloud.com>
2026-02-03 20:35:15 +0900

    chore: clang-format
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>

--- src/agnocast_ioctl_wrapper/src/topic_list.cpp
+++ src/agnocast_ioctl_wrapper/src/topic_list.cpp
@@ -1,18 +1,15 @@
 #include "agnocast_ioctl.hpp"
-#include "rclcpp/rclcpp.hpp"
 
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-#include <algorithm>
-#include <iostream>
-#include <optional>
+#include <cstdlib>
+#include <cstring>
 #include <string>
 #include <vector>
 
-namespace
-{
+extern "C" {
 
 bool is_service_topic(const std::string & topic_name)
 {
@@ -23,8 +20,10 @@ bool is_service_topic(const std::string & topic_name)
   return topic_name.compare(0, prefix.size(), prefix) == 0;
 }
 
-std::optional<std::vector<std::string>> get_agnocast_topics()
+char ** get_agnocast_topics(int * topic_count)
 {
+  *topic_count = 0;
+
   int fd = open("/dev/agnocast", O_RDONLY);
   if (fd < 0) {
     if (errno == ENOENT) {
@@ -32,101 +31,58 @@ std::optional<std::vector<std::string>> get_agnocast_topics()
     } else {
       perror("Failed to open /dev/agnocast");
     }
-    return std::nullopt;
+    return nullptr;
   }
 
-  std::array<char, MAX_TOPIC_NUM * TOPIC_NAME_BUFFER_SIZE> agnocast_topic_buffer{};
+  char * agnocast_topic_buffer =
+    static_cast<char *>(malloc(MAX_TOPIC_NUM * TOPIC_NAME_BUFFER_SIZE));
 
   ioctl_topic_list_args topic_list_args = {};
-  topic_list_args.topic_name_buffer_addr = reinterpret_cast<uint64_t>(agnocast_topic_buffer.data());
+  topic_list_args.topic_name_buffer_addr = reinterpret_cast<uint64_t>(agnocast_topic_buffer);
   if (ioctl(fd, AGNOCAST_GET_TOPIC_LIST_CMD, &topic_list_args) < 0) {
     perror("AGNOCAST_GET_TOPIC_LIST_CMD failed");
+    free(agnocast_topic_buffer);
     close(fd);
-    return std::nullopt;
+    return nullptr;
+  }
+
+  if (topic_list_args.ret_topic_num == 0) {
+    free(agnocast_topic_buffer);
+    close(fd);
+    return nullptr;
   }
 
   std::vector<std::string> agnocast_topics;
   agnocast_topics.reserve(topic_list_args.ret_topic_num);
   for (uint32_t i = 0; i < topic_list_args.ret_topic_num; i++) {
-    std::string topic_name = agnocast_topic_buffer.data() + i * TOPIC_NAME_BUFFER_SIZE;
+    std::string topic_name = agnocast_topic_buffer + i * TOPIC_NAME_BUFFER_SIZE;
     // Filter out topics used for service/client.
     if (!is_service_topic(topic_name)) {
       agnocast_topics.push_back(std::move(topic_name));
+      (*topic_count)++;
     }
   }
 
+  free(agnocast_topic_buffer);
   close(fd);
-  return agnocast_topics;
-}
 
-std::vector<std::string> get_ros2_topics()
-{
-  rclcpp::init(0, nullptr);
-  auto node = rclcpp::Node::make_shared("agnocast_topic_list_all");
-
-  // wait for ROS 2 to start
-  constexpr std::chrono::milliseconds wait_time(200);
-  std::this_thread::sleep_for(wait_time);
-
-  auto ros2_topic_names_and_types = node->get_topic_names_and_types();
-  std::vector<std::string> ros2_topics(ros2_topic_names_and_types.size());
-  std::transform(
-    ros2_topic_names_and_types.begin(), ros2_topic_names_and_types.end(), ros2_topics.begin(),
-    [](const auto & pair) { return pair.first; });
-
-  rclcpp::shutdown();
-  return ros2_topics;
-}
-
-}  // namespace
-
-extern "C" int topic_list()
-{
-  std::optional<std::vector<std::string>> agnocast_topics_opt = get_agnocast_topics();
-  if (!agnocast_topics_opt.has_value()) {
-    return -1;
-  }
-  std::vector<std::string> & agnocast_topics = agnocast_topics_opt.value();
-
-  auto ros2_topics = get_ros2_topics();
-
-  // ======== Print topics ========
-  // Before printing, merge topics that exist in both vectors to avoid duplicates.
-
-  std::sort(agnocast_topics.begin(), agnocast_topics.end());
-  std::sort(ros2_topics.begin(), ros2_topics.end());
-
-  size_t agnocast_topic_index = 0;
-  size_t ros2_topic_index = 0;
-  while (agnocast_topic_index < agnocast_topics.size() || ros2_topic_index < ros2_topics.size()) {
-    if (agnocast_topic_index == agnocast_topics.size()) {
-      for (size_t i = ros2_topic_index; i < ros2_topics.size(); i++) {
-        std::cout << ros2_topics[i] << std::endl;
-      }
-      break;
-    }
-
-    if (ros2_topic_index == ros2_topics.size()) {
-      for (size_t i = agnocast_topic_index; i < agnocast_topics.size(); i++) {
-        std::cout << agnocast_topics[i] << " (Agnocast enabled)" << std::endl;
-      }
-      break;
-    }
-
-    int ret = agnocast_topics[agnocast_topic_index].compare(ros2_topics[ros2_topic_index]);
-    if (ret == 0) {
-      std::cout << agnocast_topics[agnocast_topic_index] << " (Agnocast enabled)" << std::endl;
-      agnocast_topic_index++;
-      ros2_topic_index++;
-    } else if (ret < 0) {
-      // This branch is executed when only Agnocast Subscription exists.
-      std::cout << agnocast_topics[agnocast_topic_index] << " (Agnocast enabled)" << std::endl;
-      agnocast_topic_index++;
-    } else {
-      std::cout << ros2_topics[ros2_topic_index] << std::endl;
-      ros2_topic_index++;
-    }
+  char ** topic_array = static_cast<char **>(malloc(*topic_count * sizeof(char *)));
+  if (topic_array == nullptr) {
+    return nullptr;
   }
 
-  return 0;
+  for (size_t i = 0; i < *topic_count; i++) {
+    topic_array[i] = static_cast<char *>(malloc((agnocast_topics[i].size() + 1) * sizeof(char)));
+    if (!topic_array[i]) {
+      for (size_t j = 0; j < i; j++) {
+        free(topic_array[j]);
+      }
+      free(topic_array);
+      return nullptr;
+    }
+    std::strcpy(topic_array[i], agnocast_topics[i].c_str());
+  }
+  return topic_array;
 }
+
+}  // extern "C"
--- src/ros2agnocast/ros2agnocast/verb/list_agnocast.py
+++ src/ros2agnocast/ros2agnocast/verb/list_agnocast.py
@@ -1,11 +1,40 @@
 import ctypes
+from ros2cli.node.strategy import NodeStrategy
+from ros2topic.api import get_topic_names_and_types
 from ros2topic.verb import VerbExtension
 
 class ListAgnocastVerb(VerbExtension):
     "Output a list of available topics including Agnocast"
 
     def main(self, *, args):
-        lib = ctypes.CDLL("libagnocast_ioctl_wrapper.so")
-        lib.topic_list.argtypes = []
-        lib.topic_list.restype = ctypes.c_int
-        lib.topic_list()
+        with NodeStrategy(None) as node:
+            lib = ctypes.CDLL("libagnocast_ioctl_wrapper.so")
+            lib.get_agnocast_topics.argtypes = [ctypes.POINTER(ctypes.c_int)]
+            lib.get_agnocast_topics.restype = ctypes.POINTER(ctypes.POINTER(ctypes.c_char))
+            lib.free_agnocast_topics.argtypes = [ctypes.POINTER(ctypes.POINTER(ctypes.c_char)), ctypes.c_int]
+            lib.free_agnocast_topics.restype = None
+            
+            # Get Agnocast topics
+            topic_count = ctypes.c_int()
+            agnocast_topic_array = lib.get_agnocast_topics(ctypes.byref(topic_count))
+            agnocast_topics = []
+            for i in range(topic_count.value):
+                topic_ptr = ctypes.cast(agnocast_topic_array[i], ctypes.c_char_p)
+                topic_name = topic_ptr.value.decode('utf-8')
+                agnocast_topics.append(topic_name)
+            if topic_count.value != 0:
+                lib.free_agnocast_topics(agnocast_topic_array, topic_count)
+            # Get ros2 topics
+            ros2_topics_data = get_topic_names_and_types(node=node)
+            ros2_topics = [name for name, _ in ros2_topics_data]
+
+            ########################################################################
+            # Print topic list
+            ########################################################################
+            agnocast_topics_set = set(agnocast_topics)
+            ros2_topics_set = set(ros2_topics)
+
+            for topic in sorted(agnocast_topics_set | ros2_topics_set):
+                suffix = " (Agnocast enabled)" if topic in agnocast_topics_set else ""
+                print(f"{topic}{suffix}")
+
