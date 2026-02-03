a74fc46ca5015a4638252249a6b8cc91f106e2ff
TetsuKawa <kawaguchitnon@icloud.com>
2026-02-04 01:02:01 +0900

    feat: update topic list display
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>

--- src/ros2agnocast/ros2agnocast/verb/list_agnocast.py
+++ src/ros2agnocast/ros2agnocast/verb/list_agnocast.py
@@ -3,6 +3,16 @@ from ros2cli.node.strategy import NodeStrategy
 from ros2topic.api import get_topic_names_and_types
 from ros2topic.verb import VerbExtension
 
+class TopicInfoRet(ctypes.Structure):
+    _fields_ = [
+        ("node_name", ctypes.c_char * 256),
+        ("qos_depth", ctypes.c_uint32),
+        ("qos_is_transient_local", ctypes.c_bool),
+        # Agnocast does not natively support reliability configuration,
+        # but this field is required to pass the QoS profile to the ROS 2 bridge.
+        ("qos_is_reliable", ctypes.c_bool),
+        ("is_bridge", ctypes.c_bool),
+    ]
 class ListAgnocastVerb(VerbExtension):
     "Output a list of available topics including Agnocast"
 
@@ -13,6 +23,41 @@ class ListAgnocastVerb(VerbExtension):
             lib.get_agnocast_topics.restype = ctypes.POINTER(ctypes.POINTER(ctypes.c_char))
             lib.free_agnocast_topics.argtypes = [ctypes.POINTER(ctypes.POINTER(ctypes.c_char)), ctypes.c_int]
             lib.free_agnocast_topics.restype = None
+
+            # For bridge detection, we need to get nodes by topic
+            lib.get_agnocast_sub_nodes.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_int)]
+            lib.get_agnocast_sub_nodes.restype = ctypes.POINTER(TopicInfoRet)
+            lib.get_agnocast_pub_nodes.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_int)]
+            lib.get_agnocast_pub_nodes.restype = ctypes.POINTER(TopicInfoRet)
+            lib.free_agnocast_topic_info_ret.argtypes = [ctypes.POINTER(TopicInfoRet)]
+            lib.free_agnocast_topic_info_ret.restype = None
+
+            def is_topic_bridged(topic_name):
+                topic_name_bytes = topic_name.encode('utf-8')
+
+                # Check Agnocast subscribers
+                sub_count = ctypes.c_int()
+                sub_array = lib.get_agnocast_sub_nodes(topic_name_bytes, ctypes.byref(sub_count))
+                
+                try:
+                    if any(sub_array[i].is_bridge for i in range(sub_count.value)):
+                        return True
+                finally:
+                    if sub_count.value > 0 and sub_array:
+                        lib.free_agnocast_topic_info_ret(sub_array)
+
+                # Check Agnocast publishers
+                pub_count = ctypes.c_int()
+                pub_array = lib.get_agnocast_pub_nodes(topic_name_bytes, ctypes.byref(pub_count))
+                
+                try:
+                    if any(pub_array[i].is_bridge for i in range(pub_count.value)):
+                        return True
+                finally:
+                    if pub_count.value > 0 and pub_array:
+                        lib.free_agnocast_topic_info_ret(pub_array)
+
+                return False
             
             # Get Agnocast topics
             topic_count = ctypes.c_int()
@@ -24,6 +69,7 @@ class ListAgnocastVerb(VerbExtension):
                 agnocast_topics.append(topic_name)
             if topic_count.value != 0:
                 lib.free_agnocast_topics(agnocast_topic_array, topic_count)
+            
             # Get ros2 topics
             ros2_topics_data = get_topic_names_and_types(node=node)
             ros2_topics = [name for name, _ in ros2_topics_data]
@@ -35,6 +81,13 @@ class ListAgnocastVerb(VerbExtension):
             ros2_topics_set = set(ros2_topics)
 
             for topic in sorted(agnocast_topics_set | ros2_topics_set):
-                suffix = " (Agnocast enabled)" if topic in agnocast_topics_set else ""
+                if topic in agnocast_topics_set and topic not in ros2_topics_set:
+                    suffix = " (Agnocast enabled)"
+                elif topic in ros2_topics_set and topic not in agnocast_topics_set:
+                    suffix = ""
+                else:
+                    if is_topic_bridged(topic):
+                        suffix = " (Agnocast enabled, bridged)"
+                    else:
+                        suffix = " (ERROR: Agnocast ros2 mismatch)"
                 print(f"{topic}{suffix}")
-
