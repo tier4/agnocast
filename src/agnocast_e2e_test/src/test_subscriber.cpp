c52b5ad2218397b2470a5f99ad7699c4b6d8657d
Yutaro Kobayashi <129580202+kobayu858@users.noreply.github.com>
2026-02-04 21:05:18 +0900

    feat(agnocast_e2e_test): add r2a bridge test (#970)
    * feat: R2A test
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * remove: result log
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: check incompatible QoS
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: add comment
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: copilot
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: unity
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: remove log file
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * fix: e2e_2to2
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    * refactor: revert comment
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    
    ---------
    
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>
    Co-authored-by: Koichi Imai <45482193+Koichi98@users.noreply.github.com>

--- src/agnocast_e2e_test/src/test_subscriber.cpp
+++ src/agnocast_e2e_test/src/test_subscriber.cpp
@@ -8,22 +8,26 @@ using std::placeholders::_1;
 class TestSubscriber : public rclcpp::Node
 {
   agnocast::Subscription<std_msgs::msg::Int64>::SharedPtr sub_;
-  uint64_t count_;
-  uint64_t target_sub_num_;
   bool forever_;
+  int64_t target_end_id_;
+  int target_end_count_;
+  int received_end_count_ = 0;
 
   void callback(const agnocast::ipc_shared_ptr<std_msgs::msg::Int64> & message)
   {
     RCLCPP_INFO(this->get_logger(), "Receiving %ld.", message->data);
 
-    count_++;
-    if (count_ == target_sub_num_) {
-      RCLCPP_INFO(this->get_logger(), "All messages received. Shutting down.");
-      std::cout << std::flush;
-      sleep(3);  // HACK: wait for other nodes in the same container
+    if (message->data == target_end_id_) {
+      received_end_count_++;
 
-      if (!forever_) {
-        rclcpp::shutdown();
+      if (received_end_count_ >= target_end_count_) {
+        RCLCPP_INFO(this->get_logger(), "All messages received. Shutting down.");
+        std::cout << std::flush;
+        sleep(3);  // HACK: wait for other nodes in the same container
+
+        if (!forever_) {
+          rclcpp::shutdown();
+        }
       }
     }
   }
@@ -33,18 +37,18 @@ public:
   {
     this->declare_parameter<int64_t>("qos_depth", 10);
     this->declare_parameter<bool>("transient_local", true);
-    this->declare_parameter<int64_t>("sub_num", 10);
     this->declare_parameter<bool>("forever", false);
+    this->declare_parameter<int64_t>("target_end_id", 0);
+    this->declare_parameter<int>("target_end_count", 1);
     forever_ = this->get_parameter("forever").as_bool();
+    target_end_id_ = this->get_parameter("target_end_id").as_int();
+    target_end_count_ = this->get_parameter("target_end_count").as_int();
 
     int64_t qos_depth = this->get_parameter("qos_depth").as_int();
     rclcpp::QoS qos = rclcpp::QoS(rclcpp::KeepLast(qos_depth));
     if (this->get_parameter("transient_local").as_bool()) {
       qos.transient_local();
     }
-
-    count_ = 0;
-    target_sub_num_ = this->get_parameter("sub_num").as_int();
 
     auto cbg = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
     agnocast::SubscriptionOptions sub_options;
