8d94bc4ba1a76db9030089fa1563ff32e885ba0f
Koichi Imai <45482193+Koichi98@users.noreply.github.com>
2026-02-04 16:27:27 +0900

    feat(agnocastlib): signal handler for agnocast::Node (#964)
    * add signal_handler
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    
    * delete unnecessary comments
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    
    * Apply suggestion from @atsushi421
    
    Co-authored-by: atsushi yano <55824710+atsushi421@users.noreply.github.com>
    
    * Update src/agnocastlib/src/node/agnocast_only_multi_threaded_executor.cpp
    
    Co-authored-by: atsushi yano <55824710+atsushi421@users.noreply.github.com>
    
    * fix
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    
    ---------
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    Co-authored-by: atsushi yano <55824710+atsushi421@users.noreply.github.com>

--- src/agnocastlib/include/agnocast/node/agnocast_only_executor.hpp
+++ src/agnocastlib/include/agnocast/node/agnocast_only_executor.hpp
@@ -16,12 +16,14 @@ class AgnocastOnlyExecutor
 protected:
   std::atomic_bool spinning_;
   int epoll_fd_;
+  int shutdown_event_fd_;
   pid_t my_pid_;
 
   std::mutex ready_agnocast_executables_mutex_;
   std::vector<AgnocastExecutable> ready_agnocast_executables_;
 
-  bool get_next_agnocast_executable(AgnocastExecutable & agnocast_executable, const int timeout_ms);
+  bool get_next_agnocast_executable(
+    AgnocastExecutable & agnocast_executable, const int timeout_ms, bool & shutdown_detected);
   bool get_next_ready_agnocast_executable(AgnocastExecutable & agnocast_executable);
   void execute_agnocast_executable(AgnocastExecutable & agnocast_executable);
 
@@ -31,6 +33,7 @@ public:
   virtual ~AgnocastOnlyExecutor();
 
   virtual void spin() = 0;
+  void cancel();
 
   // Implemented align to unify the API with rclcpp::Executor
   void add_node(const std::shared_ptr<agnocast::Node> & node);
