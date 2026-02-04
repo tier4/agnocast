79dce3fefaf1592ba1533549488abde48b71c11a
atsushi421 <atsushi.yano.2@tier4.jp>
2026-02-05 08:49:07 +0900

    feat(agnocastlib): add Synchronizer and ExactTime sync policy
    Add multi-message synchronization components:
    
    - Signal9: thread-safe 9-message signal dispatcher
    - Synchronizer: policy-based multi-message synchronizer (2-9 inputs)
    - PolicyBase: common type aliases for sync policies
    - ExactTime: groups messages by exact timestamp match
    
    Includes unit tests for ExactTime (timestamp matching, queue size, drop
    callback) and Synchronizer (compile tests for 2-9 inputs, callback
    signatures, add routing).

---
+++ src/agnocastlib/include/agnocast/message_filters/sync_policies/exact_time.hpp
@@ -0,0 +1,191 @@
+#pragma once
+
+#include "agnocast/message_filters/signal9.hpp"
+#include "agnocast/message_filters/synchronizer.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <message_filters/connection.h>
+#include <message_filters/message_traits.h>
+#include <message_filters/null_types.h>
+
+#include <cassert>
+#include <map>
+#include <mutex>
+#include <string>
+#include <tuple>
+
+namespace agnocast
+{
+namespace message_filters
+{
+namespace sync_policies
+{
+
+using ::message_filters::Connection;
+using ::message_filters::NullType;
+
+template <
+  typename M0, typename M1, typename M2 = NullType, typename M3 = NullType, typename M4 = NullType,
+  typename M5 = NullType, typename M6 = NullType, typename M7 = NullType, typename M8 = NullType>
+struct ExactTime : public PolicyBase<M0, M1, M2, M3, M4, M5, M6, M7, M8>
+{
+  using Sync = Synchronizer<ExactTime>;
+  using Super = PolicyBase<M0, M1, M2, M3, M4, M5, M6, M7, M8>;
+  using Messages = typename Super::Messages;
+  using Signal = typename Super::Signal;
+  using Events = typename Super::Events;
+  using RealTypeCount = typename Super::RealTypeCount;
+  using M0Event = typename Super::M0Event;
+  using M1Event = typename Super::M1Event;
+  using M2Event = typename Super::M2Event;
+  using M3Event = typename Super::M3Event;
+  using M4Event = typename Super::M4Event;
+  using M5Event = typename Super::M5Event;
+  using M6Event = typename Super::M6Event;
+  using M7Event = typename Super::M7Event;
+  using M8Event = typename Super::M8Event;
+  using Tuple = Events;
+
+  explicit ExactTime(uint32_t queue_size) : parent_(nullptr), queue_size_(queue_size) {}
+
+  ExactTime(const ExactTime & e) { *this = e; }
+
+  // cppcheck-suppress operatorEqVarError  // drop_signal_ is not copyable (contains std::mutex)
+  ExactTime & operator=(const ExactTime & rhs)
+  {
+    parent_ = rhs.parent_;
+    queue_size_ = rhs.queue_size_;
+    last_signal_time_ = rhs.last_signal_time_;
+    tuples_ = rhs.tuples_;
+
+    return *this;
+  }
+
+  void initParent(Sync * parent) { parent_ = parent; }
+
+  template <int i>
+  void add(const typename std::tuple_element<i, Events>::type & evt)
+  {
+    assert(parent_);
+
+    namespace mt = ::message_filters::message_traits;
+
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    Tuple & t = tuples_[mt::TimeStamp<typename std::tuple_element<i, Messages>::type>::value(
+      *evt.getMessage())];
+    std::get<i>(t) = evt;
+
+    checkTuple(t);
+  }
+
+  template <class C>
+  Connection registerDropCallback(const C & callback)
+  {
+    return drop_signal_.addCallback(callback);
+  }
+
+  template <class C>
+  Connection registerDropCallback(C & callback)
+  {
+    return drop_signal_.addCallback(callback);
+  }
+
+  template <class C, typename T>
+  Connection registerDropCallback(const C & callback, T * t)
+  {
+    return drop_signal_.addCallback(callback, t);
+  }
+
+  template <class C, typename T>
+  Connection registerDropCallback(C & callback, T * t)
+  {
+    return drop_signal_.addCallback(callback, t);
+  }
+
+private:
+  // assumes mutex_ is already locked
+  void checkTuple(Tuple & t)
+  {
+    namespace mt = ::message_filters::message_traits;
+
+    bool full = true;
+    full = full && static_cast<bool>(std::get<0>(t).getMessage());
+    full = full && static_cast<bool>(std::get<1>(t).getMessage());
+    full = full && (RealTypeCount::value > 2 ? static_cast<bool>(std::get<2>(t).getMessage())
+                                             : true);  // NOLINT
+    full = full && (RealTypeCount::value > 3 ? static_cast<bool>(std::get<3>(t).getMessage())
+                                             : true);  // NOLINT
+    full = full && (RealTypeCount::value > 4 ? static_cast<bool>(std::get<4>(t).getMessage())
+                                             : true);  // NOLINT
+    full = full && (RealTypeCount::value > 5 ? static_cast<bool>(std::get<5>(t).getMessage())
+                                             : true);  // NOLINT
+    full = full && (RealTypeCount::value > 6 ? static_cast<bool>(std::get<6>(t).getMessage())
+                                             : true);  // NOLINT
+    full = full && (RealTypeCount::value > 7 ? static_cast<bool>(std::get<7>(t).getMessage())
+                                             : true);  // NOLINT
+    full = full && (RealTypeCount::value > 8 ? static_cast<bool>(std::get<8>(t).getMessage())
+                                             : true);  // NOLINT
+
+    if (full) {
+      parent_->signal(
+        std::get<0>(t), std::get<1>(t), std::get<2>(t), std::get<3>(t), std::get<4>(t),
+        std::get<5>(t), std::get<6>(t), std::get<7>(t), std::get<8>(t));
+
+      last_signal_time_ = mt::TimeStamp<M0>::value(*std::get<0>(t).getMessage());
+
+      tuples_.erase(last_signal_time_);
+
+      clearOldTuples();
+    }
+
+    if (queue_size_ > 0) {
+      while (tuples_.size() > queue_size_) {
+        Tuple & t2 = tuples_.begin()->second;
+        drop_signal_.call(
+          std::get<0>(t2), std::get<1>(t2), std::get<2>(t2), std::get<3>(t2), std::get<4>(t2),
+          std::get<5>(t2), std::get<6>(t2), std::get<7>(t2), std::get<8>(t2));
+        tuples_.erase(tuples_.begin());
+      }
+    }
+  }
+
+  // assumes mutex_ is already locked
+  void clearOldTuples()
+  {
+    auto it = tuples_.begin();
+    auto end = tuples_.end();
+    for (; it != end;) {
+      if (it->first <= last_signal_time_) {
+        auto old = it;
+        ++it;
+
+        Tuple & t = old->second;
+        drop_signal_.call(
+          std::get<0>(t), std::get<1>(t), std::get<2>(t), std::get<3>(t), std::get<4>(t),
+          std::get<5>(t), std::get<6>(t), std::get<7>(t), std::get<8>(t));
+        tuples_.erase(old);
+      } else {
+        // the map is sorted by time, so we can ignore anything after this if this one's time is ok
+        break;
+      }
+    }
+  }
+
+private:
+  Sync * parent_;
+
+  uint32_t queue_size_;
+  using M_TimeToTuple = std::map<rclcpp::Time, Tuple>;
+  M_TimeToTuple tuples_;
+  rclcpp::Time last_signal_time_;
+
+  Signal drop_signal_;
+
+  std::mutex mutex_;
+};
+
+}  // namespace sync_policies
+}  // namespace message_filters
+}  // namespace agnocast
---
+++ src/agnocastlib/test/unit/message_filters/test_synchronizer.cpp
@@ -0,0 +1,482 @@
+#include "agnocast/message_filters/synchronizer.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <gtest/gtest.h>
+
+#include <array>
+
+using namespace agnocast::message_filters;
+using namespace std::placeholders;
+using ::message_filters::NullFilter;
+using ::message_filters::NullType;
+
+struct Header
+{
+  rclcpp::Time stamp;
+};
+
+struct Msg
+{
+  Header header;
+  int data;
+};
+
+using MsgPtr = agnocast::ipc_shared_ptr<Msg>;
+using MsgConstPtr = agnocast::ipc_shared_ptr<Msg const>;
+
+// Test helper to create ipc_shared_ptr for testing without kernel support
+template <typename T>
+agnocast::ipc_shared_ptr<T> make_test_ipc_shared_ptr(T * ptr)
+{
+  return agnocast::ipc_shared_ptr<T>(ptr, "test_topic", 0, -1);
+}
+
+template <
+  typename M0, typename M1, typename M2 = NullType, typename M3 = NullType, typename M4 = NullType,
+  typename M5 = NullType, typename M6 = NullType, typename M7 = NullType, typename M8 = NullType>
+struct NullPolicy : public PolicyBase<M0, M1, M2, M3, M4, M5, M6, M7, M8>
+{
+  using Sync = Synchronizer<NullPolicy>;
+  using Super = PolicyBase<M0, M1, M2, M3, M4, M5, M6, M7, M8>;
+  using Messages = typename Super::Messages;
+  using Signal = typename Super::Signal;
+  using Events = typename Super::Events;
+  using RealTypeCount = typename Super::RealTypeCount;
+
+  NullPolicy()
+  {
+    for (size_t i = 0; i < RealTypeCount::value; ++i) {
+      added_[i] = 0;
+    }
+  }
+
+  void initParent(Sync *) {}
+
+  template <int i>
+  void add(const typename std::tuple_element<i, Events>::type &)
+  {
+    ++added_.at(i);
+  }
+
+  std::array<int32_t, RealTypeCount::value> added_;
+};
+
+using Policy2 = NullPolicy<Msg, Msg>;
+using Policy3 = NullPolicy<Msg, Msg, Msg>;
+using Policy4 = NullPolicy<Msg, Msg, Msg, Msg>;
+using Policy5 = NullPolicy<Msg, Msg, Msg, Msg, Msg>;
+using Policy6 = NullPolicy<Msg, Msg, Msg, Msg, Msg, Msg>;
+using Policy7 = NullPolicy<Msg, Msg, Msg, Msg, Msg, Msg, Msg>;
+using Policy8 = NullPolicy<Msg, Msg, Msg, Msg, Msg, Msg, Msg, Msg>;
+using Policy9 = NullPolicy<Msg, Msg, Msg, Msg, Msg, Msg, Msg, Msg, Msg>;
+
+TEST(AgnocastSynchronizer, compile2)
+{
+  NullFilter<Msg> f0, f1;
+  Synchronizer<Policy2> sync(f0, f1);
+}
+
+TEST(AgnocastSynchronizer, compile3)
+{
+  NullFilter<Msg> f0, f1, f2;
+  Synchronizer<Policy3> sync(f0, f1, f2);
+}
+
+TEST(AgnocastSynchronizer, compile4)
+{
+  NullFilter<Msg> f0, f1, f2, f3;
+  Synchronizer<Policy4> sync(f0, f1, f2, f3);
+}
+
+TEST(AgnocastSynchronizer, compile5)
+{
+  NullFilter<Msg> f0, f1, f2, f3, f4;
+  Synchronizer<Policy5> sync(f0, f1, f2, f3, f4);
+}
+
+TEST(AgnocastSynchronizer, compile6)
+{
+  NullFilter<Msg> f0, f1, f2, f3, f4, f5;
+  Synchronizer<Policy6> sync(f0, f1, f2, f3, f4, f5);
+}
+
+TEST(AgnocastSynchronizer, compile7)
+{
+  NullFilter<Msg> f0, f1, f2, f3, f4, f5, f6;
+  Synchronizer<Policy7> sync(f0, f1, f2, f3, f4, f5, f6);
+}
+
+TEST(AgnocastSynchronizer, compile8)
+{
+  NullFilter<Msg> f0, f1, f2, f3, f4, f5, f6, f7;
+  Synchronizer<Policy8> sync(f0, f1, f2, f3, f4, f5, f6, f7);
+}
+
+TEST(AgnocastSynchronizer, compile9)
+{
+  NullFilter<Msg> f0, f1, f2, f3, f4, f5, f6, f7, f8;
+  Synchronizer<Policy9> sync(f0, f1, f2, f3, f4, f5, f6, f7, f8);
+}
+
+void function2(const MsgConstPtr &, const MsgConstPtr &)
+{
+}
+void function3(const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &)
+{
+}
+void function4(const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &)
+{
+}
+void function5(
+  const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &,
+  const MsgConstPtr &)
+{
+}
+void function6(
+  const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &,
+  const MsgConstPtr &, const MsgConstPtr &)
+{
+}
+void function7(
+  const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &,
+  const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &)
+{
+}
+void function8(
+  const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &,
+  const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &)
+{
+}
+void function9(
+  const MsgConstPtr &, MsgConstPtr, const MsgConstPtr &, MsgConstPtr, const MsgConstPtr &,
+  MsgConstPtr, const MessageEvent<Msg const> &, const MessageEvent<Msg const> &,
+  const MsgConstPtr &)
+{
+}
+
+TEST(AgnocastSynchronizer, compileFunction2)
+{
+  Synchronizer<Policy2> sync;
+  sync.registerCallback(function2);
+}
+
+TEST(AgnocastSynchronizer, compileFunction3)
+{
+  Synchronizer<Policy3> sync;
+  sync.registerCallback(function3);
+}
+
+TEST(AgnocastSynchronizer, compileFunction4)
+{
+  Synchronizer<Policy4> sync;
+  sync.registerCallback(function4);
+}
+
+TEST(AgnocastSynchronizer, compileFunction5)
+{
+  Synchronizer<Policy5> sync;
+  sync.registerCallback(function5);
+}
+
+TEST(AgnocastSynchronizer, compileFunction6)
+{
+  Synchronizer<Policy6> sync;
+  sync.registerCallback(function6);
+}
+
+TEST(AgnocastSynchronizer, compileFunction7)
+{
+  Synchronizer<Policy7> sync;
+  sync.registerCallback(function7);
+}
+
+TEST(AgnocastSynchronizer, compileFunction8)
+{
+  Synchronizer<Policy8> sync;
+  sync.registerCallback(function8);
+}
+
+TEST(AgnocastSynchronizer, compileFunction9)
+{
+  Synchronizer<Policy9> sync;
+  sync.registerCallback(function9);
+}
+
+struct MethodHelper
+{
+  void method2(const MsgConstPtr &, const MsgConstPtr &) {}
+  void method3(const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &) {}
+  void method4(const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &)
+  {
+  }
+  void method5(
+    const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &,
+    const MsgConstPtr &)
+  {
+  }
+  void method6(
+    const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &,
+    const MsgConstPtr &, const MsgConstPtr &)
+  {
+  }
+  void method7(
+    const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &,
+    const MsgConstPtr &, const MsgConstPtr &, const MsgConstPtr &)
+  {
+  }
+  void method8(
+    const MsgConstPtr &, MsgConstPtr, const MsgConstPtr &, MsgConstPtr, const MsgConstPtr &,
+    MsgConstPtr, const MessageEvent<Msg const> &, const MessageEvent<Msg const> &)
+  {
+  }
+};
+
+TEST(AgnocastSynchronizer, compileMethod2)
+{
+  MethodHelper h;
+  Synchronizer<Policy2> sync;
+  sync.registerCallback(&MethodHelper::method2, &h);
+}
+
+TEST(AgnocastSynchronizer, compileMethod3)
+{
+  MethodHelper h;
+  Synchronizer<Policy3> sync;
+  sync.registerCallback(&MethodHelper::method3, &h);
+}
+
+TEST(AgnocastSynchronizer, compileMethod4)
+{
+  MethodHelper h;
+  Synchronizer<Policy4> sync;
+  sync.registerCallback(&MethodHelper::method4, &h);
+}
+
+TEST(AgnocastSynchronizer, compileMethod5)
+{
+  MethodHelper h;
+  Synchronizer<Policy5> sync;
+  sync.registerCallback(&MethodHelper::method5, &h);
+}
+
+TEST(AgnocastSynchronizer, compileMethod6)
+{
+  MethodHelper h;
+  Synchronizer<Policy6> sync;
+  sync.registerCallback(&MethodHelper::method6, &h);
+}
+
+TEST(AgnocastSynchronizer, compileMethod7)
+{
+  MethodHelper h;
+  Synchronizer<Policy7> sync;
+  sync.registerCallback(&MethodHelper::method7, &h);
+}
+
+TEST(AgnocastSynchronizer, compileMethod8)
+{
+  MethodHelper h;
+  Synchronizer<Policy8> sync;
+  sync.registerCallback(&MethodHelper::method8, &h);
+}
+
+// Helper class to manage test messages (prevents memory leaks)
+class TestMsgManager
+{
+public:
+  MsgConstPtr createMsg()
+  {
+    Msg * raw = new Msg();
+    msgs_.push_back(raw);
+    return make_test_ipc_shared_ptr(const_cast<Msg const *>(raw));
+  }
+
+  ~TestMsgManager()
+  {
+    for (auto * msg : msgs_) {
+      delete msg;
+    }
+  }
+
+private:
+  std::vector<Msg *> msgs_;
+};
+
+TEST(AgnocastSynchronizer, add2)
+{
+  Synchronizer<Policy2> sync;
+  TestMsgManager mgr;
+  auto m = mgr.createMsg();
+
+  ASSERT_EQ(sync.added_[0], 0);
+  sync.add<0>(m);
+  ASSERT_EQ(sync.added_[0], 1);
+  ASSERT_EQ(sync.added_[1], 0);
+  sync.add<1>(m);
+  ASSERT_EQ(sync.added_[1], 1);
+}
+
+TEST(AgnocastSynchronizer, add3)
+{
+  Synchronizer<Policy3> sync;
+  TestMsgManager mgr;
+  auto m = mgr.createMsg();
+
+  ASSERT_EQ(sync.added_[0], 0);
+  sync.add<0>(m);
+  ASSERT_EQ(sync.added_[0], 1);
+  ASSERT_EQ(sync.added_[1], 0);
+  sync.add<1>(m);
+  ASSERT_EQ(sync.added_[1], 1);
+  ASSERT_EQ(sync.added_[2], 0);
+  sync.add<2>(m);
+  ASSERT_EQ(sync.added_[2], 1);
+}
+
+TEST(AgnocastSynchronizer, add4)
+{
+  Synchronizer<Policy4> sync;
+  TestMsgManager mgr;
+  auto m = mgr.createMsg();
+
+  ASSERT_EQ(sync.added_[0], 0);
+  sync.add<0>(m);
+  ASSERT_EQ(sync.added_[0], 1);
+  ASSERT_EQ(sync.added_[1], 0);
+  sync.add<1>(m);
+  ASSERT_EQ(sync.added_[1], 1);
+  ASSERT_EQ(sync.added_[2], 0);
+  sync.add<2>(m);
+  ASSERT_EQ(sync.added_[2], 1);
+  ASSERT_EQ(sync.added_[3], 0);
+  sync.add<3>(m);
+  ASSERT_EQ(sync.added_[3], 1);
+}
+
+TEST(AgnocastSynchronizer, add5)
+{
+  Synchronizer<Policy5> sync;
+  TestMsgManager mgr;
+  auto m = mgr.createMsg();
+
+  ASSERT_EQ(sync.added_[0], 0);
+  sync.add<0>(m);
+  ASSERT_EQ(sync.added_[0], 1);
+  ASSERT_EQ(sync.added_[1], 0);
+  sync.add<1>(m);
+  ASSERT_EQ(sync.added_[1], 1);
+  ASSERT_EQ(sync.added_[2], 0);
+  sync.add<2>(m);
+  ASSERT_EQ(sync.added_[2], 1);
+  ASSERT_EQ(sync.added_[3], 0);
+  sync.add<3>(m);
+  ASSERT_EQ(sync.added_[3], 1);
+  ASSERT_EQ(sync.added_[4], 0);
+  sync.add<4>(m);
+  ASSERT_EQ(sync.added_[4], 1);
+}
+
+TEST(AgnocastSynchronizer, add6)
+{
+  Synchronizer<Policy6> sync;
+  TestMsgManager mgr;
+  auto m = mgr.createMsg();
+
+  for (int i = 0; i < 6; ++i) {
+    ASSERT_EQ(sync.added_[i], 0);
+  }
+  sync.add<0>(m);
+  ASSERT_EQ(sync.added_[0], 1);
+  sync.add<1>(m);
+  ASSERT_EQ(sync.added_[1], 1);
+  sync.add<2>(m);
+  ASSERT_EQ(sync.added_[2], 1);
+  sync.add<3>(m);
+  ASSERT_EQ(sync.added_[3], 1);
+  sync.add<4>(m);
+  ASSERT_EQ(sync.added_[4], 1);
+  sync.add<5>(m);
+  ASSERT_EQ(sync.added_[5], 1);
+}
+
+TEST(AgnocastSynchronizer, add7)
+{
+  Synchronizer<Policy7> sync;
+  TestMsgManager mgr;
+  auto m = mgr.createMsg();
+
+  for (int i = 0; i < 7; ++i) {
+    ASSERT_EQ(sync.added_[i], 0);
+  }
+  sync.add<0>(m);
+  ASSERT_EQ(sync.added_[0], 1);
+  sync.add<1>(m);
+  ASSERT_EQ(sync.added_[1], 1);
+  sync.add<2>(m);
+  ASSERT_EQ(sync.added_[2], 1);
+  sync.add<3>(m);
+  ASSERT_EQ(sync.added_[3], 1);
+  sync.add<4>(m);
+  ASSERT_EQ(sync.added_[4], 1);
+  sync.add<5>(m);
+  ASSERT_EQ(sync.added_[5], 1);
+  sync.add<6>(m);
+  ASSERT_EQ(sync.added_[6], 1);
+}
+
+TEST(AgnocastSynchronizer, add8)
+{
+  Synchronizer<Policy8> sync;
+  TestMsgManager mgr;
+  auto m = mgr.createMsg();
+
+  for (int i = 0; i < 8; ++i) {
+    ASSERT_EQ(sync.added_[i], 0);
+  }
+  sync.add<0>(m);
+  ASSERT_EQ(sync.added_[0], 1);
+  sync.add<1>(m);
+  ASSERT_EQ(sync.added_[1], 1);
+  sync.add<2>(m);
+  ASSERT_EQ(sync.added_[2], 1);
+  sync.add<3>(m);
+  ASSERT_EQ(sync.added_[3], 1);
+  sync.add<4>(m);
+  ASSERT_EQ(sync.added_[4], 1);
+  sync.add<5>(m);
+  ASSERT_EQ(sync.added_[5], 1);
+  sync.add<6>(m);
+  ASSERT_EQ(sync.added_[6], 1);
+  sync.add<7>(m);
+  ASSERT_EQ(sync.added_[7], 1);
+}
+
+TEST(AgnocastSynchronizer, add9)
+{
+  Synchronizer<Policy9> sync;
+  TestMsgManager mgr;
+  auto m = mgr.createMsg();
+
+  for (int i = 0; i < 9; ++i) {
+    ASSERT_EQ(sync.added_[i], 0);
+  }
+  sync.add<0>(m);
+  ASSERT_EQ(sync.added_[0], 1);
+  sync.add<1>(m);
+  ASSERT_EQ(sync.added_[1], 1);
+  sync.add<2>(m);
+  ASSERT_EQ(sync.added_[2], 1);
+  sync.add<3>(m);
+  ASSERT_EQ(sync.added_[3], 1);
+  sync.add<4>(m);
+  ASSERT_EQ(sync.added_[4], 1);
+  sync.add<5>(m);
+  ASSERT_EQ(sync.added_[5], 1);
+  sync.add<6>(m);
+  ASSERT_EQ(sync.added_[6], 1);
+  sync.add<7>(m);
+  ASSERT_EQ(sync.added_[7], 1);
+  sync.add<8>(m);
+  ASSERT_EQ(sync.added_[8], 1);
+}
