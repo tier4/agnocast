134b4d7294e11e16c7bcd606d5fa805dde540d1b
Koichi <koichi.imai.2@tier4.jp>
2026-02-08 03:20:43 +0900

    fix and add scripts
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>

--- src/agnocast_sample_application/src/no_rclcpp_client.cpp
+++ src/agnocast_sample_application/src/no_rclcpp_client.cpp
@@ -1,66 +1,51 @@
 #include "agnocast/agnocast.hpp"
 #include "agnocast_sample_interfaces/srv/sum_int_array.hpp"
 
-#include <chrono>
+#include <thread>
 
 using namespace std::chrono_literals;
 
 constexpr size_t ARRAY_SIZE = 100;
 
-class NoRclcppClient : public agnocast::Node
-{
-  using ServiceT = agnocast_sample_interfaces::srv::SumIntArray;
-  using ClientT = agnocast::Client<ServiceT>;
-
-  typename ClientT::SharedPtr client_;
-  agnocast::TimerBase::SharedPtr timer_;
-  bool request_sent_;
-
-  void timer_callback()
-  {
-    if (!client_->service_is_ready()) {
-      RCLCPP_INFO(this->get_logger(), "Service not available, waiting...");
-      return;
-    }
-
-    if (request_sent_) {
-      return;
-    }
-    request_sent_ = true;
-
-    // Send first request with callback
-    auto request1 = client_->borrow_loaned_request();
-    for (size_t i = 1; i <= ARRAY_SIZE; ++i) {
-      request1->data.push_back(i);
-    }
-    client_->async_send_request(std::move(request1), [this](ClientT::SharedFuture future) {
-      RCLCPP_INFO(this->get_logger(), "Result1: %ld", future.get()->sum);
-    });
-
-    // Send second request and wait for result
-    auto request2 = client_->borrow_loaned_request();
-    for (size_t i = 0; i < ARRAY_SIZE; ++i) {
-      request2->data.push_back(i);
-    }
-    auto future = client_->async_send_request(std::move(request2));
-    RCLCPP_INFO(this->get_logger(), "Result2: %ld", future.get()->sum);
-  }
-
-public:
-  explicit NoRclcppClient() : Node("no_rclcpp_client"), request_sent_(false)
-  {
-    client_ = this->create_client<ServiceT>("sum_int_array");
-    timer_ = this->create_wall_timer(1s, std::bind(&NoRclcppClient::timer_callback, this));
-  }
-};
-
 int main(int argc, char * argv[])
 {
   agnocast::init(argc, argv);
-  agnocast::AgnocastOnlySingleThreadedExecutor executor;
-  auto node = std::make_shared<NoRclcppClient>();
-  executor.add_node(node);
-  executor.spin();
+
+  auto node = std::make_shared<agnocast::Node>("no_rclcpp_client");
+  std::thread spin_thread([node]() {
+    agnocast::AgnocastOnlySingleThreadedExecutor executor;
+    executor.add_node(node);
+    executor.spin();
+  });
+
+  using ServiceT = agnocast_sample_interfaces::srv::SumIntArray;
+  auto client = node->create_client<ServiceT>("sum_int_array");
+
+  while (!client->wait_for_service(1s)) {
+    if (!agnocast::ok()) {
+      RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for the service. Exiting.");
+      return 0;
+    }
+    RCLCPP_INFO(node->get_logger(), "Service not available, waiting again...");
+  }
+
+  auto request1 = client->borrow_loaned_request();
+  for (size_t i = 1; i <= ARRAY_SIZE; ++i) {
+    request1->data.push_back(i);
+  }
+  client->async_send_request(
+    std::move(request1), [node](agnocast::Client<ServiceT>::SharedFuture future) {
+      RCLCPP_INFO(node->get_logger(), "Result1: %ld", future.get()->sum);
+    });
+
+  auto request2 = client->borrow_loaned_request();
+  for (size_t i = 0; i < ARRAY_SIZE; ++i) {
+    request2->data.push_back(i);
+  }
+  auto future = client->async_send_request(std::move(request2));
+  RCLCPP_INFO(node->get_logger(), "Result2: %ld", future.get()->sum);
+
+  spin_thread.join();
   agnocast::shutdown();
   return 0;
 }
