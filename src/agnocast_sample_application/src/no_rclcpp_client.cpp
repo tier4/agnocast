1119796ed539feab56de783425fa5edc501384c6
Koichi Imai <45482193+Koichi98@users.noreply.github.com>
2026-02-09 14:54:03 +0900

    feat(sample_app): implement service/client for agnocast::Node (#1012)
    * add shutdown and ok func
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * Update Client and Server to support agnocast::Node
    
    * call agnocast::ok()
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * implement service/client for agnocast::Node
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * fix and add scripts
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * add shutdown and ok func
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * fix to atomic exchange
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * fix contention
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * fix signalsafe
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * fix
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * delete agnocast::ok and added comments
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * fix to skip check for context when agnocast::Node
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * delete ok,shutdown and add comment
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * same as one above
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * add static_assert
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * fix context semantics to align with rclcpp
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * fix comments
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * clang-format
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    
    ---------
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    Co-authored-by: bdm-k <kokusyunn@gmail.com>

---
+++ src/agnocast_sample_application/src/no_rclcpp_client.cpp
@@ -0,0 +1,48 @@
+#include "agnocast/agnocast.hpp"
+#include "agnocast_sample_interfaces/srv/sum_int_array.hpp"
+
+#include <thread>
+
+using namespace std::chrono_literals;
+
+constexpr size_t ARRAY_SIZE = 100;
+
+int main(int argc, char * argv[])
+{
+  agnocast::init(argc, argv);
+
+  auto node = std::make_shared<agnocast::Node>("no_rclcpp_client");
+  std::thread spin_thread([node]() {
+    agnocast::AgnocastOnlySingleThreadedExecutor executor;
+    executor.add_node(node);
+    executor.spin();
+  });
+
+  using ServiceT = agnocast_sample_interfaces::srv::SumIntArray;
+  auto client = node->create_client<ServiceT>("sum_int_array");
+
+  // TODO(Koichi98): Add agnocast::ok() check here
+  while (!client->wait_for_service(1s)) {
+    RCLCPP_INFO(node->get_logger(), "Service not available, waiting again...");
+  }
+
+  auto request1 = client->borrow_loaned_request();
+  for (size_t i = 1; i <= ARRAY_SIZE; ++i) {
+    request1->data.push_back(i);
+  }
+  client->async_send_request(
+    std::move(request1), [node](agnocast::Client<ServiceT>::SharedFuture future) {
+      RCLCPP_INFO(node->get_logger(), "Result1: %ld", future.get()->sum);
+    });
+
+  auto request2 = client->borrow_loaned_request();
+  for (size_t i = 0; i < ARRAY_SIZE; ++i) {
+    request2->data.push_back(i);
+  }
+  auto future = client->async_send_request(std::move(request2));
+  RCLCPP_INFO(node->get_logger(), "Result2: %ld", future.get()->sum);
+
+  spin_thread.join();
+  // TODO(Koichi98): Add agnocast::shutdown() here
+  return 0;
+}
