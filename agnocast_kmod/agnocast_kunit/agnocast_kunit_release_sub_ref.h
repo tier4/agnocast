182596d773e8c6a6f07a5ad9d794a9cea5a56a69
Koichi Imai <koichi.imai.2@tier4.jp>
2026-02-12 16:42:45 +0900

    add ref count test for adding multiple refcounts
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- agnocast_kmod/agnocast_kunit/agnocast_kunit_release_sub_ref.h
+++ agnocast_kmod/agnocast_kunit/agnocast_kunit_release_sub_ref.h
@@ -6,10 +6,12 @@
     KUNIT_CASE(test_case_release_sub_ref_no_message),     \
     KUNIT_CASE(test_case_release_sub_ref_no_pubsub_id),   \
     KUNIT_CASE(test_case_release_sub_ref_last_reference), \
-    KUNIT_CASE(test_case_release_sub_ref_multi_reference)
+    KUNIT_CASE(test_case_release_sub_ref_multi_reference), \
+    KUNIT_CASE(test_case_increment_rc_already_referenced)
 
 void test_case_release_sub_ref_no_topic(struct kunit * test);
 void test_case_release_sub_ref_no_message(struct kunit * test);
 void test_case_release_sub_ref_no_pubsub_id(struct kunit * test);
 void test_case_release_sub_ref_last_reference(struct kunit * test);
 void test_case_release_sub_ref_multi_reference(struct kunit * test);
+void test_case_increment_rc_already_referenced(struct kunit * test);
