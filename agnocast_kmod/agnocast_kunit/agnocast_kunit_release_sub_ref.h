10e84b376754fe28391c7c38af489b263e0e8304
Koichi Imai <koichi.imai.2@tier4.jp>
2026-02-12 17:01:35 +0900

    fix
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- agnocast_kmod/agnocast_kunit/agnocast_kunit_release_sub_ref.h
+++ agnocast_kmod/agnocast_kunit/agnocast_kunit_release_sub_ref.h
@@ -1,11 +1,11 @@
 #pragma once
 #include <kunit/test.h>
 
-#define TEST_CASES_RELEASE_SUB_REF                        \
-  KUNIT_CASE(test_case_release_sub_ref_no_topic),         \
-    KUNIT_CASE(test_case_release_sub_ref_no_message),     \
-    KUNIT_CASE(test_case_release_sub_ref_no_pubsub_id),   \
-    KUNIT_CASE(test_case_release_sub_ref_last_reference), \
+#define TEST_CASES_RELEASE_SUB_REF                         \
+  KUNIT_CASE(test_case_release_sub_ref_no_topic),          \
+    KUNIT_CASE(test_case_release_sub_ref_no_message),      \
+    KUNIT_CASE(test_case_release_sub_ref_no_pubsub_id),    \
+    KUNIT_CASE(test_case_release_sub_ref_last_reference),  \
     KUNIT_CASE(test_case_release_sub_ref_multi_reference), \
     KUNIT_CASE(test_case_increment_rc_already_referenced)
 
