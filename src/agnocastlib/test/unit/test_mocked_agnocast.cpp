6fccf90891de505e9da165ad19020c5d91f6180e
Koichi <koichi.imai.2@tier4.jp>
2026-02-18 23:06:45 +0900

    add get_bridge mock function to return Bridge off
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>

--- src/agnocastlib/test/unit/test_mocked_agnocast.cpp
+++ src/agnocastlib/test/unit/test_mocked_agnocast.cpp
@@ -50,6 +50,11 @@ union ioctl_publish_msg_args publish_core(
   publish_core_mock_called_count++;
   return ioctl_publish_msg_args{};  // Dummy value
 }
+
+BridgeMode get_bridge_mode()
+{
+  return BridgeMode::Off;  // Skip MQ sending in tests
+}
 }  // namespace agnocast
 
 // =========================================
