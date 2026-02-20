12a0d4ee979e0f63952f141a5cdcfadfce6bd01f
kobayu858 <yutaro.kobayashi.2@tier4.jp>
2026-02-20 20:21:04 +0900

    refactor: all path test
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

--- src/agnocast_sample_application/src/no_rclcpp_take_subscriber.cpp
+++ src/agnocast_sample_application/src/no_rclcpp_take_subscriber.cpp
@@ -10,6 +10,32 @@ class NoRclcppTakeSubscriber : public agnocast::Node
   agnocast::PollingSubscriber<agnocast_sample_interfaces::msg::DynamicSizeArray>::SharedPtr sub_;
   agnocast::TimerBase::SharedPtr timer_;
 
+public:
+  // カバレッジテスト用に追加したコールバックグループ
+  rclcpp::CallbackGroup::SharedPtr manual_group_;
+  rclcpp::CallbackGroup::SharedPtr late_auto_group_;
+
+  explicit NoRclcppTakeSubscriber() : agnocast::Node("no_rclcpp_take_subscriber")
+  {
+    sub_ = this->create_subscription<agnocast_sample_interfaces::msg::DynamicSizeArray>(
+      "/my_topic", rclcpp::QoS(rclcpp::KeepLast(1)));
+
+    timer_ = this->create_wall_timer(1s, std::bind(&NoRclcppTakeSubscriber::timer_callback, this));
+
+    // 【テスト準備1】自動追加フラグを false にしてグループを作成
+    manual_group_ =
+      this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive, false);
+
+    RCLCPP_INFO(get_logger(), "NoRclcppTakeSubscriber started");
+  }
+
+  void create_late_group()
+  {
+    // 【テスト準備2】ノードがExecutorに追加された後に作成
+    late_auto_group_ = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+  }
+
+private:
   void timer_callback()
   {
     auto message = sub_->take_data();
@@ -19,25 +45,45 @@ class NoRclcppTakeSubscriber : public agnocast::Node
         message->data.size());
     }
   }
+};
 
+// =========================================================
+// 【追加】protectedなメソッドを呼び出すためのテスト用Executor
+// =========================================================
+class TestExecutor : public agnocast::AgnocastOnlyMultiThreadedExecutor
+{
 public:
-  explicit NoRclcppTakeSubscriber() : agnocast::Node("no_rclcpp_take_subscriber")
+  void trigger_add_groups()
   {
-    sub_ = this->create_subscription<agnocast_sample_interfaces::msg::DynamicSizeArray>(
-      "/my_topic", rclcpp::QoS(rclcpp::KeepLast(1)));
-
-    timer_ = this->create_wall_timer(1s, std::bind(&NoRclcppTakeSubscriber::timer_callback, this));
-
-    RCLCPP_INFO(get_logger(), "NoRclcppTakeSubscriber started");
+    // 子クラスの中であれば、protectedな親メソッドを呼び出せる
+    this->add_callback_groups_from_nodes_associated_to_executor();
   }
 };
 
 int main(int argc, char ** argv)
 {
   agnocast::init(argc, argv);
-  agnocast::AgnocastOnlyMultiThreadedExecutor executor;
+
+  // 通常のエグゼキュータではなく、作成したテスト用エグゼキュータを使用する
+  TestExecutor executor;
   auto node = std::make_shared<NoRclcppTakeSubscriber>();
+
+  // ---------------------------------------------------------
+  // 既存のパス3: add_nodeによる自動追加
+  // ---------------------------------------------------------
   executor.add_node(node);
+
+  // ---------------------------------------------------------
+  // 未網羅パス1: add_callback_group の直接呼び出し
+  // ---------------------------------------------------------
+  executor.add_callback_group(node->manual_group_, node->get_node_base_interface());
+
+  // ---------------------------------------------------------
+  // 未網羅パス2: テスト用エグゼキュータを経由して呼び出す
+  // ---------------------------------------------------------
+  node->create_late_group();
+  executor.trigger_add_groups();
+
   executor.spin();
   return 0;
 }
